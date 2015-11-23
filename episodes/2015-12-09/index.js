af5ace8c72719e2e29f15644a3232131f12d8bba
Kent C. Dodds <kent@doddsfamily.us>
2015-11-23 09:51:22 -0700

    Remove unnecessary date

--- episodes/2015-12-09/index.js
+++ episodes/2015-12-09/index.js
@@ -1,7 +1,6 @@
 export default {
   title: 'The past, present, and future of JavaScript',
   hangoutUrl: 'https://plus.google.com/events/c39cuc7ueppus41ajobpr9qt6cg',
-  date: '2015-12-09',
   guests: [
     {name: 'Brendan Eich', twitter: 'brendaneich'},
   ],
