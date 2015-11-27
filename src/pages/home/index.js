7eaca90bb335a7ff586d0355e0b9e379ffb861b1
Kent C. Dodds <kent@doddsfamily.us>
2015-11-26 23:47:12 -0700

    Create the page wrapper for use for future pages

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,6 +3,7 @@ import ReactDOMServer from 'react-dom/server'
 import {groupBy} from 'lodash'
 import moment from 'moment'
 
+import Page from '../page'
 import Header from './header'
 import SubscribeIconGroupSection from './sections/subscribe-icon-group'
 import PreviousEpisodeSection from './sections/previous-episodes'
@@ -13,7 +14,6 @@ import SocialIconGroupSection from './sections/social-icon-group'
 import GoogleAnalyticsScript from './scripts/google-analytics'
 import FeatureShowScript from './scripts/feature-show'
 
-
 import episodes from '../../../episodes'
 
 const today = moment()
@@ -22,56 +22,47 @@ const episodeGroups = groupBy(episodes, e => {
   return yesterday.diff(e.date) < 0 ? 'future' : 'past'
 })
 
-
-/* eslint max-len:[2, 161] */ // TODO fix this
-function App() {
+function Home({futureEpisodes = [], pastEpisodes = []}) {
   return (
-    <html>
-      <head lang="en">
-        <title>JavaScript Air</title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="google-site-verification" content="85n8ZBk_3hSeShlRmsVJXgDolakFG4UsMJgpy3mQyPs" />
-        <meta name="theme-color" content="#155674" />
-        <link rel="shortcut icon" type="image/png" href="favicon.ico"/>
-        <link rel="stylesheet" href="styles.css" />
-        <link rel="stylesheet" href="resources/font/font.css" />
-      </head>
-      <body>
-        <Header />
+    <Page>
+      <Header />
 
-        <hr />
+      <hr />
 
-        <SubscribeIconGroupSection />
+      <SubscribeIconGroupSection />
 
-        <hr />
+      <hr />
 
-        <EpisodesSection episodes={episodeGroups.future} />
+      <EpisodesSection episodes={futureEpisodes} />
 
-        <hr />
+      {futureEpisodes.length ? <hr /> : ''}
 
-        <PreviousEpisodeSection episodes={episodeGroups.past} />
+      <PreviousEpisodeSection episodes={pastEpisodes} />
 
-        {episodes.past ? <hr /> : ''}
+      {pastEpisodes.length ? <hr /> : ''}
 
-        <HostSection />
+      <HostSection />
 
-        <hr />
+      <hr />
 
-        <PanelistsSection />
+      <PanelistsSection />
 
-        <hr />
+      <hr />
 
-        <SocialIconGroupSection />
+      <SocialIconGroupSection />
 
-        <GoogleAnalyticsScript />
-        <FeatureShowScript />
-      </body>
-    </html>
+      <GoogleAnalyticsScript />
+      <FeatureShowScript />
+    </Page>
   )
 }
 
-const string = ReactDOMServer.renderToStaticMarkup(<App />)
+const string = ReactDOMServer.renderToStaticMarkup(
+  <Home
+    futureEpisodes={episodeGroups.future}
+    pastEpisodes={episodeGroups.past}
+  />
+)
 
 console.log(string) // eslint-disable-line no-console
 
---
+++ src/pages/page.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default Page
+
+function Page({
+  children,
+  title = 'JavaScript Air',
+} = {}) {
+  return (
+    <html>
+      <head lang="en">
+        <title>{title}</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="85n8ZBk_3hSeShlRmsVJXgDolakFG4UsMJgpy3mQyPs" />
+        <meta name="theme-color" content="#155674" />
+        <link rel="shortcut icon" type="image/png" href="favicon.ico"/>
+        <link rel="stylesheet" href="styles.css" />
+        <link rel="stylesheet" href="resources/font/font.css" />
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
+
