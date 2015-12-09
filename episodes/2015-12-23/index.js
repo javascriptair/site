575ea132e7ffaf4615f97ef8304a76600ae35d02
Kent C. Dodds <kent@doddsfamily.us>
2015-12-08 22:20:58 -0700

    Adding @hzoo for the babel show ref - #14

--- episodes/2015-12-23/index.js
+++ episodes/2015-12-23/index.js
@@ -1,3 +1,14 @@
 export default {
+  title: 'Babel, the JavaScript Compiler',
   hangoutUrl: 'https://plus.google.com/events/coi555uqlhl7vsdspafjj05k4dc',
+  guests: [
+    {name: 'Henry Zhu', twitter: '_henryzhu'},
+  ],
+  description: `
+    Tons of people use Babel. With the release of Babel 6,
+    it is now a language platform as well as a transpiler.
+    Join us as we talk about the ecosystem of Babel plugins,
+    ASTs, and all of the neat stuff that you can do with this
+    amazing tool with some of the core contributors to the project.
+  `,
 }
