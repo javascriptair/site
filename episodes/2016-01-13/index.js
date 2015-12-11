c9aa9b52064e24a6d7b248ac6eafe1c8848195f5
Kent C. Dodds <kent@doddsfamily.us>
2015-12-11 09:35:48 -0700

    Add web performance episode (#16) with @nikmd23

---
+++ episodes/2016-01-13/index.js
@@ -0,0 +1,15 @@
+export default {
+  title: 'Web Performance',
+  hangoutUrl: 'https://plus.google.com/events/c06t5m6an80s3aracj7erm18lkc',
+  guests: [
+    {name: 'Nik Molnar', twitter: 'nikmd23'},
+  ],
+  description: `
+    A lot goes into building applications that perform well
+    on all devices your users want to use. +Nik Molnar will
+    join us to talk about the nitty-gritty details of web
+    performance and things you need to consider when building
+    applications for all consumption mediums and geolocations.
+  `,
+}
+
