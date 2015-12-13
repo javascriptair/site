974ba7efa5cc736e1c14f6030a45b74281d8f5cc
Kent C. Dodds <kent@doddsfamily.us>
2015-12-13 15:08:00 -0700

    Add Chakra Episode, ref #12

---
+++ episodes/2016-01-27/index.js
@@ -0,0 +1,49 @@
+/* eslint max-len:0 */
+
+export default {
+  title: `Chakra, Microsoft's Open Source JavaScript Engine`,
+  hangoutUrl: '',
+  guests: [
+  ],
+  description: `
+    Microsoft has been making some pretty awesome
+    moves in the world of open source recently.
+    At JSConf in early December, it was announced that
+    Chackra, Microsoft's JavaScript Engine
+    that powers the Microsoft Edge browser
+    (with better support for ES6 than babel or Chrome),
+    will be open sourced. Join us as we talk about
+    Chakra with some of the subject matter experts!
+  `,
+  podbeanId: '',
+  youTubeId: '',
+  host: {},
+  panelists: [
+    {
+      twitter: 'dan_abramov',
+    },
+    {
+      twitter: 'drboolean',
+    },
+    {
+      twitter: 'getify',
+    },
+    {
+      twitter: 'kwuchu',
+    },
+    {
+      twitter: 'linclark',
+    },
+    {
+      twitter: 'mzabriskie',
+    },
+    {
+      twitter: 'pamasaur',
+    },
+    {
+      twitter: 'tylermcginnis33',
+    },
+  ],
+}
+
+
