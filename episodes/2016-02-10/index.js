aa7d1c52ede1b9a0fa15ed3a3ca18fccd06c6d9e
Kent C. Dodds <kent@doddsfamily.us>
2015-12-14 15:35:15 -0800

    Adding ESLint show information with @feross, @gyandeeps, @ilyavolodin, and @xjamundx

---
+++ episodes/2016-02-10/index.js
@@ -0,0 +1,61 @@
+/* eslint max-len:0 */
+
+export default {
+  title: `ESLint`,
+  guests: [
+    {
+      name: 'Gyandeep Singh',
+      twitter: 'gyandeeps',
+    },
+    {
+      name: 'Ilya Volodin',
+      twitter: 'i_volodin',
+    },
+    {
+      name: 'Feross Aboukhadijeh',
+      twitter: 'feross',
+    },
+    {
+      name: 'Jamund Ferguson',
+      twitter: 'xjamundx',
+    },
+  ],
+  description: `
+    Let's talk about what [ESLint](http://eslint.org/) is, how it works,
+    and how it can be super useful for your project.
+    We'll be joined by ESLint committers and the author
+    of [the Standard library](http://npm.im/standard).
+  `,
+  hangoutId: 'cms6et0sffpkf92nucv1nf70c74',
+  youTubeId: 'sEXow0FPE3Q',
+  podbeanId: '',
+  host: {},
+  panelists: [
+    {
+      twitter: 'dan_abramov',
+    },
+    {
+      twitter: 'drboolean',
+    },
+    {
+      twitter: 'getify',
+    },
+    {
+      twitter: 'kwuchu',
+    },
+    {
+      twitter: 'linclark',
+    },
+    {
+      twitter: 'mzabriskie',
+    },
+    {
+      twitter: 'pamasaur',
+    },
+    {
+      twitter: 'tylermcginnis33',
+    },
+  ],
+}
+
+
