87856847656cf1f7c8fad8c0fdaa5bced20a7016
Kent C. Dodds <kent@doddsfamily.us>
2015-12-14 07:12:41 -0800

    Adding show #9 with @gaearon and @DrBoolean.
    Will add @fogus later if he expresses interested to join in :-)

--- episodes/2015-12-30/index.js
+++ episodes/2015-12-30/index.js
@@ -1,4 +1,48 @@
+/* eslint max-len:0 */
+
 export default {
+  title: `Functional and Immutable Design Patterns in JavaScript`,
+  guests: [
+    {
+      name: 'Dan Abramov',
+      twitter: 'dan_abramov',
+      imgSrc: '/resources/panelists/dan_abramov.png',
+    },
+    {
+      name: 'Brian Lonsdorf',
+      twitter: 'drboolean',
+      imgSrc: '/resources/panelists/drboolean.png',
+    },
+  ],
+  description: `
+    Functional programming have been greatly facilitated with ES6.
+    We're going to talk about the how and why of functional
+    programming and immutable design patterns in JavaScript.
+  `,
   hangoutId: 'c6gcf8f0nirc5t83homjb5trquc',
   youTubeId: '82M9fKe7hiw',
+  podbeanId: '',
+  host: {},
+  panelists: [
+    {
+      twitter: 'getify',
+    },
+    {
+      twitter: 'kwuchu',
+    },
+    {
+      twitter: 'linclark',
+    },
+    {
+      twitter: 'mzabriskie',
+    },
+    {
+      twitter: 'pamasaur',
+    },
+    {
+      twitter: 'tylermcginnis33',
+    },
+  ],
 }
+
+
