3035e9abe8e56b07b265eca90ac3fe5e80aec54e
Kent C. Dodds <kent@doddsfamily.us>
2015-12-16 11:44:08 -0800

    Add links, tips, and picks to recent show

--- episodes/2015-12-16/index.js
+++ episodes/2015-12-16/index.js
@@ -1,9 +1,47 @@
+/* eslint max-len:0 */
+
 export default {
   title: 'Learning and Developing JavaScript',
   hangoutId: 'c2bgue075m3us9cemis03iivrag',
   guests: [
-    {name: 'Ashley G. Williams', twitter: 'ag_dubs'},
-    {name: 'Kyle Simpson', twitter: 'getify', imgSrc: '/resources/panelists/getify.png'},
+    {
+      name: 'Ashley G. Williams',
+      twitter: 'ag_dubs',
+      links: [
+        `[If you wish to learn ES6 from scratch you must first invent the universe](https://www.youtube.com/watch?v=DN4yLZB1vUQ) at [JSconf2015](http://2015.jsconf.us/)`,
+        `[Breakfast Repo](https://github.com/ashleygwilliams/breakfast-repo)`,
+      ],
+      tips: [
+        `Investigate the programming languages you use as tools`,
+        `Learn about different programming paradigms and translate side projects into them`,
+      ],
+      picks: [
+        `[Javascript Allonge](https://leanpub.com/javascript-allonge)`,
+        `[Hello Declarative World!](http://codon.com/hello-declarative-world)[my ES6 implementation](https://github.com/ashleygwilliams/relational-node)`,
+        `[Simple Made Easy, Rich Hickey](http://www.infoq.com/presentations/Simple-Made-Easy)`,
+        `[Growing a Language, Guy Steele](https://www.youtube.com/watch?v=_ahvzDzKdB0)`,
+      ],
+    },
+    {
+      name: 'Kyle Simpson',
+      twitter: 'getify',
+      imgSrc: '/resources/panelists/getify.png',
+      links: [
+        `[MakerSquare](https://twitter.com/MakerSquare)`,
+        `[Build Your Own Angular](http://teropa.info/build-your-own-angular)`,
+        `[BannerFrom NAND to Tetris](http://www.nand2tetris.org/)`,
+        `[Thunder Plains Conference](http://thunderplainsconf.com/)`,
+      ],
+      tips: [
+        `Give yourself deadlines to learn stuff (talk to give or blog to post)`,
+        `Unplug and sharpen the ax`,
+      ],
+      picks: [
+        `[David Nolen (cognicast episode)](http://blog.cognitect.com/cognicast/093)`,
+        `[BlissfulJS](http://blissfuljs.com/)`,
+        `[ES6 & Beyond](http://shop.oreilly.com/product/0636920033769.do)`,
+      ],
+    },
   ],
   description: `
     Ashley and Kyle know a thing or two about teaching JavaScript.
@@ -11,5 +49,46 @@ export default {
     learning and developing this wonky language called JavaScript.
   `,
   youTubeId: 'uDtkEEIBsz4',
+  past: true,
+  host: {
+    tips: [
+      `Contribute to Open Source ([What open source project should I contribute to?](https://medium.com/@kentcdodds/what-open-source-project-should-i-contribute-to-7d50ecfe1cb4))`,
+    ],
+    picks: [
+      `[Slides.com](https://slides.com)`,
+      `[TodoMVC](http://todomvc.com/)`,
+    ],
+  },
+  panelists: [
+    {
+      twitter: 'drboolean',
+      tips: [
+        `Start procedural => Learn OO => Learn FP`,
+      ],
+      picks: [
+        `[Jared Tobin Recursive Types](https://medium.com/@jaredtobin/a-tour-of-some-useful-recursive-types-8fa8e423b5b9#.gwhpp43bj)`,
+        `[Hard Jones’ Computational Effects](https://github.com/computational-effects/free/blob/master/examples/http/index.js)`,
+      ],
+    },
+    {
+      twitter: 'linclark',
+      tips: [
+        `[ESLint](http://eslint.org/) use it! To start using it on large codebases, add all files to \`.eslintignore\` and then tackle files one by one. This cuts down on the number of collisions with other PRs. Look for additional plugins, like the React ESLint plugin, and you can create your own`,
+      ],
+      picks: [
+        `[Async & Performance](https://github.com/getify/You-Dont-Know-JS/tree/master/async%20%26%20performance) (plus the whole [YDKJS](https://github.com/getify/You-Dont-Know-JS) series)`,
+        `[Understanding ES6](https://leanpub.com/understandinges6/read)`,
+      ],
+    },
+    {
+      twitter: 'mzabriskie',
+      tips: [
+        `Use [ESLint](http://eslint.org/) to enforce code style.`,
+      ],
+      picks: [
+        `Foo Fighters Saint Cecilia EP (free [on iTunes](https://itunes.apple.com/us/album/saint-cecilia-ep/id1059042560))`,
+      ],
+    },
+  ],
 }
 
