81b7c099c80a5c2bab0f041d4795eb33b3f1f86e
Alex Booker <alexbooker@fastmail.im>
2015-12-22 17:17:27 +0000

    fixed panelist Twitter links

--- src/pages/home/sections/host.js
+++ src/pages/home/sections/host.js
@@ -8,7 +8,8 @@ function Host() {
     <section id="host">
       <h2>Host</h2>
       <div className="person-group">
-        <Person name="Kent C. Dodds" twitter="kentcdodds" imgSrc="resources/kentcdodds.png" />
+        <Person name="Kent C. Dodds" twitter="kentcdodds" imgSrc="resources/kentcdodds.png"
+          link="https://twitter.com/kentcdodds"/>
       </div>
       <p className="+text-center">
         <a href="https://egghead.io/instructors/kentcdodds">Egghead.io</a> instructor Kent C. Dodds
