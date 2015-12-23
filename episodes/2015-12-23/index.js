b48e6c4af8c5989d4b9c112b32e826dc43c6d055
Kent C. Dodds <kent@doddsfamily.us>
2015-12-23 12:33:50 -0700

    Add links, tips and picks to the show for today

--- episodes/2015-12-23/index.js
+++ episodes/2015-12-23/index.js
@@ -3,11 +3,50 @@ export default {
   title: 'Babel, the JavaScript Compiler',
   hangoutId: 'coi555uqlhl7vsdspafjj05k4dc',
   guests: [
-    {name: 'Henry Zhu', twitter: '_henryzhu'},
-    {name: 'Logan Smyth', twitter: 'loganfsmyth'},
-    {name: 'Amjad Masad', twitter: 'amasad'},
-    {name: 'Sebastian McKenzie', twitter: 'sebmck'},
+    {
+      name: 'Henry Zhu',
+      twitter: '_henryzhu',
+      links: [
+        `[JSCS](https://github.com/jscs-dev/node-jscs)`,
+        `[Sebastian’s JSConf Talk](https://www.youtube.com/watch?v=rKuNbEwoQfQ)`,
+      ],
+      picks: [
+        `[AST Explorer](http://astexplorer.net/)`,
+        `[babel-eslint](https://github.com/babel/babel-eslint)`,
+      ],
+    },
+    {
+      name: 'Logan Smyth',
+      twitter: 'loganfsmyth',
+      tips: [
+        `Join us on the [Babel Slack](https://slack.babeljs.io/)`,
+      ],
+    },
+    {
+      name: 'Amjad Masad',
+      twitter: 'amasad',
+      links: [
+        `[Meteor Fibers](https://meteorhacks.com/fibers-eventloop-and-meteor)`,
+      ],
+      tips: [
+        `Use interface-like design and dependency injection for testing`,
+        `Promises / async functions`,
+      ],
+      picks: [
+        `[Antifragile by Nassim Nicholas Taleb](http://www.amazon.com/dp/1400067820/)`,
+        `[Emacs](https://www.gnu.org/software/emacs/)`,
+        `[repl.it](https://repl.it)`,
+      ],
+    },
+    {
+      name: 'Sebastian McKenzie',
+      twitter: 'sebmck',
+      picks: [
+        `[Sainsbury's Chocolate Flavoured British 1% Fat Milk](https://youtu.be/1w_W5cd8qRE?t=1h6m54s)`,
+      ],
+    },
   ],
+  past: true,
   description: `
     Tons of people use Babel. With the release of Babel 6,
     it is now a language platform as well as a transpiler.
@@ -16,4 +55,43 @@ export default {
     amazing tool with some of the core contributors to the project.
   `,
   youTubeId: '1w_W5cd8qRE',
+  host: {
+    links: [
+      `[Why is Babel a Monorepo?](https://github.com/babel/babel/blob/master/doc/design/monorepo.md)`,
+      `[The Babel Plugin Handbook](https://github.com/thejameskyle/babel-plugin-handbook)`,
+    ],
+    tips: [
+      `Create dev tools for your work project.`,
+      `Look into [babel loose mode](http://www.2ality.com/2015/12/babel6-loose-mode.html)`,
+    ],
+    picks: [
+      `[Misunderstanding ES6 Modules, Upgrading Babel, tears, and a solution](https://medium.com/@kentcdodds/misunderstanding-es6-modules-upgrading-babel-tears-and-a-solution-ad2d5ab93ce0#.h01yzretx)`,
+      `[Clearing up the Babel 6 Ecosystem](https://medium.com/@jcse/clearing-up-the-babel-6-ecosystem-c7678a314bf3#.ntennmo73)`,
+      `[Google Music](https://music.google.com)`,
+    ],
+  },
+  panelists: [
+    {
+      twitter: 'dan_abramov',
+      picks: [
+        `[Tree-shaking with webpack 2 and Babel 6](http://www.2ality.com/2015/12/webpack-tree-shaking.html)`,
+      ],
+    },
+    {
+      twitter: 'getify',
+      picks: [
+        `[Matt Mullenweg to WordPress Community: “Learn JavaScript, Deeply”. Here are some resources for doing that.](http://wesbos.com/learn-javascript)`,
+        `[Reddit Thread](https://www.reddit.com/r/ProgrammerHumor/comments/3xyaos/well_f_you_too_kyle_simpson/)`,
+      ],
+    },
+    {
+      twitter: 'pamasaur',
+      tips: [
+        `Take some time to reflect on the past year/2015, [Jessica Paoli recently published hers](http://jes.sica.me/post/135727838328/2015-year-in-review).`,
+      ],
+      picks: [
+        `[Advent of code](http://adventofcode.com/)`,
+      ],
+    },
+  ],
 }
