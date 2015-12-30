4e1398a1d0db0b3b9b2c311772e29dcf910f64e7
Kent C. Dodds <kent@doddsfamily.us>
2015-12-30 12:37:59 -0700

    Update picks and tips for today's show and make future stuff easier :-)

--- episodes/2015-12-30/index.js
+++ episodes/2015-12-30/index.js
@@ -7,11 +7,29 @@ export default {
       name: 'Dan Abramov',
       twitter: 'dan_abramov',
       imgSrc: '/resources/panelists/dan_abramov.png',
+      picks: [
+        `[A General Theory of Reactivity](https://github.com/kriskowal/gtor)`,
+        `[The Future of JavaScript MVC Frameworks](http://swannodette.github.io/2013/12/17/the-future-of-javascript-mvcs/)`,
+        `[Programming in the 21st Century] (http://prog21.dadgum.com/archives.html)`,
+        `[Redux Saga](https://github.com/yelouafi/redux-saga)`,
+      ],
     },
     {
       name: 'Brian Lonsdorf',
       twitter: 'drboolean',
       imgSrc: '/resources/panelists/drboolean.png',
+      links: [
+        `[A Modern Architecture for FP](http://degoes.net/articles/modern-fp/)`,
+        `[Crazy FP words tweet](https://twitter.com/drboolean/status/682059953708773377)`,
+      ],
+      tips: [
+        `Watch [Dan's](https://twitter.com/dan_abramov) [Redux videos](https://egghead.io/series/getting-started-with-redux)`,
+        `Experiment with [ramda](http://ramdajs.com), [immutable.js](https://facebook.github.io/immutable-js/), and the Maybe Functor`,
+        `If you want to read the white papers, checkout [The Haskell Road to Logic, Maths and Programming](http://www.amazon.com/Haskell-Programming-Second-Edition-Computing/dp/0954300696)`,
+      ],
+      picks: [
+        `[ForwardJS](http://forwardjs.com/) Feb 10th @Regency Ballroom SF`,
+      ],
     },
   ],
   description: `
@@ -22,25 +40,45 @@ export default {
   hangoutId: 'c6gcf8f0nirc5t83homjb5trquc',
   youTubeId: '82M9fKe7hiw',
   podbeanId: '',
-  host: {},
+  past: true,
+  host: {
+    tips: ['Try a standing desk'],
+    picks: [
+      `[Lowes](http://www.lowes.com/)`,
+      `[The ES2016 Pipe Operator](https://github.com/mindeavor/es-pipeline-operator)`,
+      `[Learn JavaScript Arrays in Depth](https://egghead.io/playlists/learn-javascript-arrays-in-depth-dbe40331)`,
+    ],
+  },
   panelists: [
     {
       twitter: 'getify',
-    },
-    {
-      twitter: 'kwuchu',
+      tips: [
+        `[Rediscover devtools in Firefox, you’ll be pleasantly surprised ](http://www.smashingmagazine.com/2015/12/revisiting-firefox-devtools/) -- specifically WebIDE/Valence and CSS Transforms Previewer`,
+      ],
+      picks: [
+        `[JavaScript Allongé (book)](https://leanpub.com/javascriptallongesix) by [@raganwald](http://twitter.com/raganwald)`,
+        `Kris Jenkins’ blog posts on FP: [Part 1] (http://blog.jenkster.com/2015/12/what-is-functional-programming.html), [Part 2] (http://blog.jenkster.com/2015/12/which-programming-languages-are-functional.html)`,
+        `[The FP side of async programming](https://medium.com/@yelouafi/from-callback-to-future-functor-monad-6c86d9c16cb5)`,
+        `[Concurrency’s future on the web](https://medium.com/@cramforce/2016-will-be-the-year-of-concurrency-on-the-web-c39b1e99b30f)`,
+      ],
     },
     {
       twitter: 'linclark',
-    },
-    {
-      twitter: 'mzabriskie',
-    },
-    {
-      twitter: 'pamasaur',
+      tips: [
+        `git checkout - takes you back to the branch you were last on`,
+        `[hub](https://github.com/github/hub) makes it easier to work with github on the command line. [Tab completion](https://github.com/github/hub#shell-tab-completion) is especially helpful`,
+        `[fetch all PRs](https://gist.github.com/piscisaureus/3342247) if you do a lot of PR reviews, this can be really helpful. It downloads all new PRs as branches whenever you fetch`,
+      ],
+      picks: [
+        `[Jessica Kerr - Functional Principles In React](https://www.youtube.com/watch?v=1uRC3hmKQnM)`,
+      ],
     },
     {
       twitter: 'tylermcginnis33',
+      picks: [
+        `[Starters and Maintainers](http://jlongster.com/Starters-and-Maintainers)`,
+        `[Classroom Coding with Prof. Frisby](https://www.youtube.com/watch?v=h_tkIpwbsxY&list=PLK_hdtAJ4KqX0JOs_KMAmUNTNMRYhWEaC)`,
+      ],
     },
   ],
 }
