4ca45a773081ddbfd86acde716a82bf569d58439
Kent C. Dodds <kent@doddsfamily.us>
2016-01-04 17:03:23 -0700

    Add show for #22

--- episodes/2016-01-13/index.js
+++ episodes/2016-01-13/index.js
@@ -1,5 +1,117 @@
+/* eslint max-len:0 */
+
 export default {
+  title: `Firefox DevTools, React, and Redux`,
+  guests: [
+    {
+      name: 'James Long',
+      twitter: 'jlongster',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      name: 'Jordan Santell',
+      twitter: 'jsantell',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+  description: `
+    React is in your browser. Not just in web pages, but in the browser itself! Join us as we talk with developers from Firefox's Developer Tools team about how they're using React and Redux to build developer tools, and how the two make the DX (developer experience) of building developer tools better.
+  `,
   hangoutId: 'c06t5m6an80s3aracj7erm18lkc',
   youTubeId: 'wseL3Vl-Hh4',
+  podbeanId: '',
+  host: {
+    links: [
+    ],
+    tips: [
+    ],
+    picks: [
+    ],
+  },
+  panelists: [
+    {
+      twitter: 'dan_abramov',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'drboolean',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'getify',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'kwuchu',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'linclark',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'mzabriskie',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'pamasaur',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'tylermcginnis33',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
 }
+
 
--- other/schedule.md
+++ other/schedule.md
@@ -4,12 +4,12 @@
 | 2015-12-23 | [Babel, the JavaScript Compiler](/../../issues/14) |
 | 2015-12-30 | [Functional and Immutable Design Patterns in JavaScript](/../../issues/9) |
 | 2016-01-06 | [Unit Testing JavaScript](/../../issues/10) |
-| 2016-01-13 | [Web Performance](/../../issues/16) |
+| 2016-01-13 | [Firefox DevTools, React, and Redux](/../../issues/22) |
 | 2016-01-20 | [Introducing the Panel](/../../issues/4) |
 | 2016-01-27 | [Chackra, Microsoft's Open Source JS Engine](/../../issues/12) |
 | 2016-02-03 | [JavaScript Debugging](/../../issues/17) |
-| 2016-02-10 | [ESLint](/../../issues/13) |
-| 2016-02-17 | [](/../../issues/) |
+| 2016-02-10 | [ESLint](/../../issues/13) or possibly [Forward Web Summit](/../../issues/32) |
+| 2016-02-17 | Maybe [ESLint](/../../issues/13) |
 | 2016-02-24 | [](/../../issues/) |
 | 2016-03-02 | [](/../../issues/) |
 | 2016-03-09 | [](/../../issues/) |
