39bbf7fa178bfde04fa440d96177dfac5dca5aaa
Kent C. Dodds <kent@doddsfamily.us>
2016-01-07 06:54:40 -0700

    Add links, picks, and tips for #10

--- episodes/2016-01-06/index.js
+++ episodes/2016-01-06/index.js
@@ -1,10 +1,44 @@
+/* eslint max-len:0 */
 export default {
   title: 'Unit Testing JavaScript',
   hangoutId: 'cd2pvrr2mghpv56o12rnlo6i488',
   guests: [
-    {name: 'Christian Johansen', twitter: 'cjno'},
-    {name: 'Joe Eames', twitter: 'josepheames'},
-    {name: 'Ward Bell', twitter: 'wardbell'},
+    {
+      name: 'Christian Johansen',
+      twitter: 'cjno',
+      tips: [
+        `Learn about Functional Programming`,
+        `Learn about loose coupling`,
+      ],
+      picks: [
+        `[Simple made easy](http://www.infoq.com/presentations/Simple-Made-Easy)`,
+        `[Off to be the wizard](http://www.audible.com/pd/Sci-Fi-Fantasy/Off-to-Be-the-Wizard-Audiobook/B00IIS32NI)`,
+      ],
+    },
+    {
+      name: 'Joe Eames',
+      twitter: 'josepheames',
+      tips: [
+        'Learn to TDD and do it for a year',
+      ],
+      picks: [
+        `[Lambda conf](http://lambdaconf.us/)`,
+        `[Sherlock](http://www.imdb.com/title/tt1475582/)`,
+        `[Soundtrack to Star Wars Episode 7](http://www.amazon.com/gp/product/B014V6JIQK?tag=gm-search-20)`,
+        `[Kent C. Dodds](https://twitter.com/kentcdodds)`,
+      ],
+    },
+    {
+      name: 'Ward Bell',
+      twitter: 'wardbell',
+      tips: [
+        `Avoid seeing Star Wars`,
+        `Think of tests as a feature`,
+      ],
+      picks: [
+        `[Thinking fast and slow](http://amzn.com/0374533555)`,
+      ],
+    },
   ],
   description: `
     Testing in JavaScript comes with its own set of challenges.
@@ -14,5 +48,54 @@ export default {
     Joe Eames, and Christian Johansen about unit testing JavaScript.
   `,
   youTubeId: 'tfkUN8Jr9zY',
+  podbeanId: '',
+  host: {
+    links: [
+    ],
+    tips: [
+      `Test your code`,
+      `Try out [AVA](http://npm.im/ava)`,
+    ],
+    picks: [
+      `[Predictable Sherlock Holmes - Studio C](https://www.youtube.com/watch?v=hwJUuN3JUPE)`,
+      `[Libraries.io](https://libraries.io/)`,
+      `[JS Remote Conf](https://allremoteconfs.com/js-2016)`,
+    ],
+  },
+  panelists: [
+    {
+      twitter: 'getify',
+      tips: [
+        `When building a complex thing with parts that are unknown, focus first on mini prototypes of each of those unknowns.`,
+        `Mentoring / coaching is essential to learning. Self-driven learning is good but only goes part of the way.`,
+      ],
+      picks: [
+        `[Fun Fun Function - Streams](https://www.youtube.com/watch?v=UD2dZw9iHCc) by [@mpjme](https://twitter.com/mpjme)`,
+        `[Simplicity Matters](https://youtu.be/rI8tNMsozo0)`,
+      ],
+    },
+    {
+      twitter: 'drboolean',
+      tips: [
+        `Use as many pure functions as possible. Easier to test.`,
+        `Test at the highest level possible to get coverage and flexibility on implementation`,
+      ],
+      picks: [
+        `[jsverify](http://jsverify.github.io/)`,
+        `[Eric Elliot’s blog posts in general](https://medium.com/javascript-scene/what-every-unit-test-needs-f6cd34d9836d#.azdssvmzn)`,
+        `[Constraints Liberate, Liberties Constrain — Runar Bjarnason](https://www.youtube.com/watch?v=GqmsQeSzMdw)`,
+      ],
+    },
+    {
+      twitter: 'tylermcginnis33',
+      tips: [
+        `console.time & console.timeEnd`,
+      ],
+      picks: [
+        `[Egghead Redux Series Notes]( https://gist.github.com/diegoconcha/8918294bb9df69876b22)`,
+        `[The Revolution of Pure Views] (https://medium.com/@puppybits/the-revolution-of-pure-views-aed339db7da4#.ku57c7wxr)`,
+      ],
+    },
+  ],
 }
 
