8a710c4dd0d58b126c27388eea1c61a104f0bb23
Alex Booker <alexbooker@fastmail.im>
2016-01-09 21:11:44 +0000

    prepared header component for new design

--- src/pages/home/header.js
+++ src/pages/home/header.js
@@ -4,23 +4,12 @@ export default Header
 
 function Header() {
   return (
-    <header className="+text-center">
-      <div id="logo">
-        <a href="https://twitter.com/JavaScriptAir">
-          <img src="resources/logo.png" />
-        </a>
-      </div>
-      <h1>JavaScript Air</h1>
-      <p id="sub-title">
-        The <strong>live</strong> broadcast podcast
-        all about JavaScript
-      </p>
-      <p>
-        Brought to you by <a href="https://egghead.io">Egghead.io</a>
-        {' '}and{' '}
-        <a href="#sponsors" alt="Sponsors">others</a>
+    <header className="header">
+      <h1 className="header__heading">JavaScript <span>Air</span></h1>
+      <p className="header__subtext">
+        Sponsored by <a className="header__sponsor-link" href="">Egghead.io</a>, and
+         <a className="header__sponsor-link" href="" >others</a>.
       </p>
     </header>
   )
 }
-
