9dae83dcab11f530d38caae09161019476116f57
Alex Booker <alexbooker@fastmail.im>
2016-01-10 01:12:20 +0000

    made header buttons mobile first

--- src/pages/home/header.js
+++ src/pages/home/header.js
@@ -12,10 +12,8 @@ function Header() {
            <a className="header__sponsor-link" href="" >others</a>.
         </p>
         <p className="header__site-tagline">The live broadcast all about JavaScript</p>
-        <div className="header__button-group">
-          <a href="#" className="header__btn">Upcoming Shows</a>
-          <a href="#" className="header__btn">Past Shows</a>
-        </div>
+        <a href="#" className="header__btn">Upcoming Shows</a>
+        <a href="#" className="header__btn">Past Shows</a>
       </div>
     </header>
   )
