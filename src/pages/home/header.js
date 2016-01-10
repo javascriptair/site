b581d6f358bf2e5c932e0f471008de0e0da4c072
Alex Booker <alexbooker@fastmail.im>
2016-01-10 00:41:33 +0000

    basic positioning for the header component

--- src/pages/home/header.js
+++ src/pages/home/header.js
@@ -5,11 +5,13 @@ export default Header
 function Header() {
   return (
     <header className="header">
-      <h1 className="header__heading">JavaScript <span>Air</span></h1>
-      <p className="header__subtext">
-        Sponsored by <a className="header__sponsor-link" href="">Egghead.io</a>, and
-         <a className="header__sponsor-link" href="" >others</a>.
-      </p>
+      <div className="header__container">
+        <h1 className="header__heading">JavaScript <span>Air</span></h1>
+        <p className="header__subtext">
+          Sponsored by <a className="header__sponsor-link" href="">Egghead.io</a>, and
+           <a className="header__sponsor-link" href="" >others</a>.
+        </p>
+      </div>
     </header>
   )
 }
