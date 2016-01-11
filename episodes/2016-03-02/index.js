bf607ee549fd62913933381ba1939b406c06061e
Kent C. Dodds <kent@doddsfamily.us>
2016-01-11 10:31:43 -0700

    Add lodash show with @jdalton (#40) to the website

--- episodes/2016-03-02/index.js
+++ episodes/2016-03-02/index.js
@@ -1,10 +1,22 @@
 /* eslint max-len:0 */
 
 export default {
-  title: `TBA`,
+  title: `JavaScript Utils & Lodash`,
   guests: [
+    {
+      name: 'John Dalton',
+      twitter: 'jdalton',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
   ],
-  description: '',
+  description: `
+    Lodash v4 had been released (by the time this episode happens). This JavaScript library of utilities is one of the most widely used utility belts of the enormous JavaScript ecosystem. We're going to talk with John, the mastermind behind this utility (and fork of underscore) about his experience building such a ubiquitous tool and what it can do for you.
+  `,
   hangoutId: 'c143rgrflt08nui9n5h42morjbs',
   youTubeId: 'pqZGVV35BPU',
   podbeanId: '',
