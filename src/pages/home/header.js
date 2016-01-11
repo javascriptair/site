31b2b19c70af8af6fee2ec2656696ea700aa3709
Alex Booker <alexbooker@fastmail.im>
2016-01-11 16:55:13 +0000

    seperated top and bottom halves of the header
    This will enable us to give them different background colours.

--- src/pages/home/header.js
+++ src/pages/home/header.js
@@ -6,36 +6,25 @@ export default Header
 function Header() {
   return (
     <header className="header">
-
-      <div className="header__container">
-
-        {/* Header - Text based titles and subtitles */}
-        <div className="header__heading">
-
-          <h1 className="header__title">
-            Javascript <span>Air</span>
-          </h1>
-
-          <p className="header__subtext">
-            Sponsored by <a className="header__sponsor-link" href="">Egghead.io</a>
-            , and <a className="header__sponsor-link" href="" >others</a>.
-          </p>
-
-        </div>
-
-        {/* Hero - Call to action text and buttons*/}
-        <div className="header__hero">
-
-          <p className="header__tagline">The live broadcast podcast all about JavaScript</p>
-
-          <div className="btn-container header__btn-container">
-            <a href="#" className="btn btn--large btn--black">Upcoming Shows</a>
-            <a href="#" className="btn btn--large btn--black">Past Shows</a>
+      <div className="header__top-half">
+        <div className="header__container">
+          <div className="header__heading">
+            <h1 className="header__title">
+              Javascript <span>Air</span>
+            </h1>
+            <p className="header__subtext">
+              Sponsored by <a className="header__sponsor-link" href="">Egghead.io</a>
+              , and <a className="header__sponsor-link" href="" >others</a>.
+            </p>
           </div>
-
+          <div className="header__hero">
+            <p className="header__tagline">The live broadcast podcast all about JavaScript</p>
+            <div className="btn-container header__btn-container">
+              <a href="#" className="btn btn--large btn--black">Upcoming Shows</a>
+              <a href="#" className="btn btn--large btn--black">Past Shows</a>
+            </div>
+          </div>
         </div>
-
-
       </div>
 
       <div className="latest-show">
