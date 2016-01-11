dd268990b7bb760f1416cc3fac9b1fbf4dad6024
Connorelsea <connorelsea@gmail.com>
2016-01-10 22:31:35 -0600

    Added CSS reset, adjusting styles for future design.

--- src/pages/home/header.js
+++ src/pages/home/header.js
@@ -9,10 +9,9 @@ function Header() {
       <div className="header__container">
         <h1 className="header__heading">JavaScript <span>Air</span></h1>
         <p className="header__subtext">
-          Sponsored by <a className="header__sponsor-link" href="">Egghead.io</a>, and
-           <a className="header__sponsor-link" href="" >others</a>.
+          Sponsored by <a className="header__sponsor-link" href="">Egghead.io</a>, and <a className="header__sponsor-link" href="" >others</a>.
         </p>
-        <p className="header__site-tagline">The live broadcast all about JavaScript</p>
+        <p className="header__site-tagline">The live broadcast podcast all about JavaScript</p>
         <a href="#" className="header__btn">Upcoming Shows</a>
         <a href="#" className="header__btn">Past Shows</a>
       </div>
