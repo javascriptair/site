88a1021b551c65fbb0d6d3dc032b3f98349192c9
Kent C. Dodds <kent@doddsfamily.us>
2016-01-13 12:57:26 -0700

    Adding links, picks, and tips for #22

--- episodes/2016-01-13/index.js
+++ episodes/2016-01-13/index.js
@@ -7,10 +7,14 @@ export default {
       name: 'James Long',
       twitter: 'jlongster',
       links: [
+        `[XUL](https://developer.mozilla.org/en-US/docs/Mozilla/Tech/XUL)`,
+        `[react-proxy](https://github.com/gaearon/react-proxy)`,
       ],
       tips: [
+        `Use [Firefox DevTools](https://developer.mozilla.org/en-US/docs/Tools) and [tell us](https://twitter.com/FirefoxDevTools) how we can do better!`,
       ],
       picks: [
+        `[Om Next](https://github.com/omcljs/om/wiki/Quick-Start-%28om.next%29)`,
       ],
     },
     {
@@ -19,11 +23,14 @@ export default {
       links: [
       ],
       tips: [
+        `[Get involved with Firefox Developer Tools!](https://wiki.mozilla.org/DevTools/GetInvolved)`,
       ],
       picks: [
+        `[Sourcemaps in Firefox](http://jlongster.com/On-the-Road-to-Better-Sourcemaps-in-the-Firefox-Developer-Tools)`,
       ],
     },
     {
+
       name: 'Lin Clark',
       twitter: 'linclark',
       imgSrc: '/resources/panelists/linclark.png',
@@ -32,6 +39,8 @@ export default {
       tips: [
       ],
       picks: [
+        `[The five keys to a successful Google team](https://rework.withgoogle.com/blog/five-keys-to-a-successful-google-team/)`,
+        `[Consensus through Conversations](http://www.amazon.com/Consensus-Through-Conversations-High-Commitment-Decisions/dp/1576754197)`,
       ],
     },
   ],
@@ -41,87 +50,43 @@ export default {
   hangoutId: 'c06t5m6an80s3aracj7erm18lkc',
   youTubeId: 'wseL3Vl-Hh4',
   podbeanId: '',
+  past: true,
   host: {
-    links: [
-    ],
     tips: [
+      `Watch your analytics. Encourage your customers to get off an unsupported, insecure browser >IE11`,
     ],
     picks: [
+      `[Elements Components and Instances](https://medium.com/@dan_abramov/react-components-elements-and-instances-90800811f8ca)`,
     ],
   },
   panelists: [
     {
       twitter: 'dan_abramov',
       links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
+        `[React Howto](https://github.com/petehunt/react-howto)`,
+        `[React Components, Elements, and Instances](http://facebook.github.io/react/blog/2015/12/18/react-components-elements-and-instances.html)`,
+        `[Minimal API Surface Area](https://www.youtube.com/watch?v=4anAwXYqLG8)`,
       ],
       picks: [
+        `[Lodash 4 is out!](https://github.com/lodash/lodash/releases/tag/4.0.0)`,
+        `[Lazarus](https://www.youtube.com/watch?v=y-JqH1M4Ya8)`,
       ],
     },
     {
       twitter: 'getify',
       links: [
+        `[SpiderMonkey improves Function.prototype.bind](https://bugzilla.mozilla.org/show_bug.cgi?id=1000780#c38)`,
+        `[v8 improves Function.prototype.bind](http://benediktmeurer.de/2015/12/25/a-new-approach-to-function-prototype-bind/)`,
       ],
       tips: [
+        `use “palm pads” to support your wrists while using laptop keyboards ([like these](http://www.amazon.com/GRIFITI-Notebook-Silicone-Reposition-Travelling/dp/B00897D3OQ/))`,
       ],
       picks: [
+        `[Microsoft Chakra is OSS now!](https://github.com/Microsoft/ChakraCore)`,
+        `[Github Social Client](https://devspace.io/) [blog post](https://blog.devspace.io/the-problems-with-github-s-news-feed-abf5e1a7aefd)`,
       ],
     },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
+    {twitter: 'mzabriskie'},
   ],
 }
 
