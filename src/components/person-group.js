701aaf03294f898d1a2769fe90f039746d4d0e23
Connorelsea <connorelsea@gmail.com>
2016-01-14 13:10:46 -0600

    Added experimental button. May iterate or remove in future

--- src/components/person-group.js
+++ src/components/person-group.js
@@ -6,9 +6,16 @@ export default PersonGroup
 function PersonGroup({people = []}) {
   return (
     <div className="person-group">
+    
       {people.map((person, index) => (
         <Person key={index} {...person} />
       ))}
+
+      <div>
+        <a className="btn btn--monochrome-gray btn--show" href="#">  Add to Calendar  </a>
+        <a className="btn btn--monochrome btn--show" href="#">  View Show  </a>
+      </div>
+
     </div>
   )
 }
