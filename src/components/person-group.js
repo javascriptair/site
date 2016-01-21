503b18748e6bf04d627794bd73648c6768da2e59
Alex Booker <alexbooker@fastmail.im>
2016-01-21 21:53:13 +0000

    Refactor icon link component thus that icons can be referenced outside of links

--- src/components/person-group.js
+++ src/components/person-group.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Person from './person'
-import {renderGraphic} from './icon-link'
+//import {renderGraphic} from './icon-link'
+import Icon from './icon'
 
 export default PersonGroup
 
@@ -15,9 +16,7 @@ function PersonGroup({people = [], hangoutUrl}) {
       <div className="person-group__btn-container">
 
         <a href={hangoutUrl} className="btn btn--monochrome-gray btn--show">
-          <svg className="rsvp-icon--btn" viewBox='0 0 24 24'>
-            {renderGraphic('calendar')}
-          </svg>
+          <Icon name='calendar'/>
         </a>
 
         <a className="btn btn--monochrome btn--show" href="#">  View Show</a>
