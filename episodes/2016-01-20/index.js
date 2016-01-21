0b520683084e2f56e77a2ab62947fcffda8de82f
Kent C. Dodds <kent@doddsfamily.us>
2016-01-20 21:28:40 -0700

    Add podbeanId for #4

--- episodes/2016-01-20/index.js
+++ episodes/2016-01-20/index.js
@@ -116,7 +116,7 @@ export default {
   `,
   hangoutId: 'ciduop5sck7su6n8f99nnctigt0',
   youTubeId: 'gxI5m_raAWg',
-  podbeanId: '',
+  podbeanId: 'mu5vn-5bebfd',
   past: true,
   host: {
     links: [
