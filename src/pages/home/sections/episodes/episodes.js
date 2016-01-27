5fde59481031d0ef0580f4a7a9ec3ba434306c34
Connorelsea <connorelsea@gmail.com>
2016-01-27 03:44:26 -0600

    Separated sorting of episodes by upcoming and future

--- src/pages/home/sections/episodes/episodes.js
+++ src/pages/home/sections/episodes/episodes.js
@@ -7,17 +7,30 @@ import * as utils from '../../../../../shared/utils'
 export default EpisodesSection
 
 function EpisodesSection({episodes = []}) {
+
+  const sortedEpisodes = episodes.sort(utils.sortEpisodes)
+
   return (
     <section id="episodes" className="episodes">
       <div className="episodes__container">
+
+        {/* Upcoming Episodes -  The first three upcoming episodes are displayed prominently */}
 
         <h2 className="episodes__title">Upcoming Episodes</h2>
 
         <div className="episodes-container">
           {
-            episodes
-              .sort(utils.sortEpisodes)
-              .map((e, i) => <Episode episodeData={e} key={i} />)
+            sortedEpisodes.slice(0, 3).map((e, i) => <Episode episodeData={e} key={i} />)
+          }
+        </div>
+
+        {/* Future Episodes - Anything after the first three is made concise*/}
+
+        <h2 className="episodes__title">Future Episodes</h2>
+
+        <div className="episodes-container--future">
+          {
+            sortedEpisodes.splice(3, sortedEpisodes.length).map((e, i) => <Episode episodeData={e} key={i} />)
           }
         </div>
 
