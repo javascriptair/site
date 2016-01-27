c6860cb4d3f3f49009bf7aeb5a921aeafb845404
Alex Booker <alexbooker@fastmail.im>
2016-01-27 19:31:35 +0000

    Make panelists section render nicer on small devices

--- src/pages/home/sections/host.js
+++ src/pages/home/sections/host.js
@@ -6,10 +6,9 @@ export default Host
 function Host() {
   return (
     <section id="host">
-      <div className="person-group">
-        <Person className="panelist" name="Kent C. Dodds" twitter="kentcdodds" imgSrc="resources/kentcdodds.png"
-          link="https://twitter.com/kentcdodds"/>
-      </div>
+      <Person className="panelist panelist--host"
+        name="Kent C. Dodds" twitter="kentcdodds" imgSrc="resources/kentcdodds.png"
+        link="https://twitter.com/kentcdodds"/>
       <p className="panelists-section__caption">
         <a className="panelists-section__link"
           href="https://egghead.io/instructors/kentcdodds">Egghead.io</a> instructor Kent C. Dodds
