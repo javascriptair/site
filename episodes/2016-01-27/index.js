199e525f9707a925796e2eadc54eb6169efd7d0b
Kent C. Dodds <kent@doddsfamily.us>
2016-01-27 17:14:14 -0700

    Add picks and tips to #12 (Chakra)

--- episodes/2016-01-27/index.js
+++ episodes/2016-01-27/index.js
@@ -7,16 +7,31 @@ export default {
     {
       name: 'Gaurav Seth',
       twitter: 'gauravseth',
+      tips: [
+        `We’re new to open source, but we’d love to hear your feedback ([ChakraCore](https://github.com/Microsoft/ChakraCore))!`,
+      ],
     },
     {
       name: 'Steve Lucco',
       imgSrc: '/episodes/2016-01-27/steveluc.png',
       link: 'https://www.linkedin.com/in/steve-lucco-b5084958',
+      tips: [
+        `Use the web programming model for as much as we can`,
+      ],
+      picks: [
+        `[Electron](http://electron.atom.io/)`,
+      ],
     },
     {
       name: 'Ed Maurer',
       imgSrc: '/episodes/2016-01-27/edmaurer.png',
       link: 'https://www.linkedin.com/in/ed-maurer-6ab4a6',
+      tips: [
+        `Check out [TypeScript](http://www.typescriptlang.org/)`,
+      ],
+      picks: [
+        `[Julia Holter](http://www.juliashammasholter.com/) album - [Have You in my Wilderness](http://www.amazon.com/Have-You-In-My-Wilderness/dp/B011DHCNTE)`,
+      ],
     },
   ],
   description: `
@@ -28,34 +43,53 @@ export default {
     (with better support for ES6 than babel or Chrome),
     will be open sourced. Join us as we talk about
     Chakra with some of the subject matter experts!
-  `,
+    `,
   podbeanId: '',
   youTubeId: '0WdFvLSEX8c',
-  host: {},
+  past: true,
+  host: {
+    links: [
+      `[Wallaby.js Sublime Plugin](http://jsair.io/wallabyjs-sublime)`,
+      `[What open source project should I contribute to?](https://medium.com/@kentcdodds/what-open-source-project-should-i-contribute-to-7d50ecfe1cb4)`,
+    ],
+    tips: [
+      `Get involved open source`,
+    ],
+    picks: [
+      `[How to become a better hacker](https://glebbahmutov.com/blog/better-hacker/) by [Gleb Bahmutov](https://twitter.com/bahmutov)`,
+    ],
+  },
   panelists: [
     {
-      twitter: 'dan_abramov',
-    },
-    {
       twitter: 'drboolean',
+      picks: [
+        `[Redux Saga](https://github.com/yelouafi/redux-saga)`,
+        `[Simon Richardson’s Free Monad implementation](https://github.com/fantasyland/fantasy-frees)`,
+        `[Brian McKenna on fantasyland](https://vimeo.com/98096838)`,
+      ],
     },
     {
       twitter: 'getify',
-    },
-    {
-      twitter: 'kwuchu',
+      picks: [
+        `[v8 gets ES6!](http://v8project.blogspot.com/2016/01/v8-release-49.html)`,
+        `[Developer Fallacies](http://www.heydonworks.com/article/developer-fallacies) by [@heydonworks](http://twitter.com/heydonworks)`,
+        `[Ember-CLI-Mirage](http://www.ember-cli-mirage.com/)`,
+      ],
     },
     {
       twitter: 'linclark',
-    },
-    {
-      twitter: 'mzabriskie',
+      links: [
+        `[Code Cartoons series on Relay](https://code-cartoons.com/)`,
+      ],
+      picks: [
+        `[React’s diff algorithm](http://calendar.perfplanet.com/2013/diff/)`,
+      ],
     },
     {
       twitter: 'pamasaur',
-    },
-    {
-      twitter: 'tylermcginnis33',
+      picks: [
+        `[Higgs JavaScript VM](https://github.com/higgsjs/Higgs)`,
+      ],
     },
   ],
 }
