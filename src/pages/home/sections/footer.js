fb2b4b70c8a8d4b02986443dc19971cd8a4bb56c
Alex Booker <alexbooker@fastmail.im>
2016-01-29 20:55:38 +0000

    Refactor links component

--- src/pages/home/sections/footer.js
+++ src/pages/home/sections/footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Icon from '../../../components/icon'
+import FooterLinks from './links'
 
 export default Footer
 
@@ -30,17 +31,7 @@ function Footer() {
           </ul>
         </div>
         <div className="footer__col">
-          <ul className="footer__nav-footer-list footer-list">
-            <li className="footer-list__item">
-              <a className="footer-list__link" href="http://suggest.javascriptair.com/">Suggest an episode</a>
-            </li>
-            <li className="footer-list__item">
-              <a className="footer-list__link" href="javascriptair+website@gmail.com">Cotact us</a>
-            </li>
-            <li className="footer-list__item">
-              <a className="footer-list__link" href="/guests">All show guests</a>
-            </li>
-          </ul>
+          <FooterLinks />
         </div>
       </div>
     </footer>
