b07d2692fb358a361acece06b48340093ad307cd
Alex Booker <alexbooker@fastmail.im>
2016-01-29 19:28:22 +0000

    Add navigation links to the footer

--- src/pages/home/sections/footer.js
+++ src/pages/home/sections/footer.js
@@ -9,6 +9,17 @@ function Footer() {
         <div className="footer__col">
         </div>
         <div className="footer__col">
+          <ul className="footer-list">
+            <li className="footer-list__item">
+              <a className="footer-list__link" href="http://suggest.javascriptair.com/">Suggest an episode</a>
+            </li>
+            <li className="footer-list__item">
+              <a className="footer-list__link" href="javascriptair+website@gmail.com">Cotact us</a>
+            </li>
+            <li className="footer-list__item">
+              <a className="footer-list__link" href="/guests">All show guests</a>
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
