335d05d8bf2abb673c6aa6cd5ac5585a3bada072
Kent C. Dodds <kent@doddsfamily.us>
2016-02-02 13:07:46 -0700

    Add Fluent discount code

--- data/deals/index.js
+++ data/deals/index.js
@@ -12,4 +12,19 @@ export default [
       the conference in downtown San Francisco.
     `,
   },
+  {
+    imgSrc: '/data/deals/fluent.png',
+    organization: 'Fluent',
+    tagline: 'Web Platform Training',
+    link: 'http://jsair.io/fluent-discount',
+    deal: 'Use code **JSAIR** for 20% off any pass',
+    expires: '2016-03-10',
+    description: `
+      Flunet is a professional web platform training conference
+      on March 7th-10th, 2016 with 2-day trainings, tutorials,
+      keynotes, and sessions. For practical training in JavaScript,
+      HTML5, CSS, and the latest web development technologies and
+      frameworks, check out Fluent Conf.
+    `,
+  },
 ]
