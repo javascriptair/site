e3a609a141896f18aa3d0f8f88dfa15afe54886e
Kent C. Dodds <kent@doddsfamily.us>
2016-02-17 19:35:27 -0700

    Add links, tips, and picks to ESLint show #13

--- episodes/2016-02-17/index.js
+++ episodes/2016-02-17/index.js
@@ -6,18 +6,55 @@ export default {
     {
       name: 'Gyandeep Singh',
       twitter: 'gyandeeps',
+      picks: [
+        `[Alex js - Catch ins ensitive, inconsiderate writing](http://alexjs.com/)`,
+        `[Proxyquire - Override dependencies during testing](https://github.com/thlorenz/proxyquire)`,
+      ],
     },
     {
       name: 'Ilya Volodin',
       twitter: 'i_volodin',
+      links: [
+        `[ESLint bot](https://github.com/Bernardstanislas/eslint-bot)`,
+        `[babel-eslint](https://github.com/babel/babel-eslint)`,
+        `[eslint-plugin-babel](https://github.com/babel/eslint-plugin-babel)`,
+        `[eslint-plugin-react](https://github.com/yannickcr/eslint-plugin-react)`,
+        `[eslint-plugin-angularjs](https://github.com/Gillespie59/eslint-plugin-angular)`,
+      ],
+      tips: [
+      ],
+      picks: [
+        `[Visual Studio Code](https://code.visualstudio.com/)`,
+        `[Iron Node](https://github.com/s-a/iron-node)`,
+        `[babel-eslint](https://github.com/babel/babel-eslint)`,
+      ],
     },
     {
       name: 'Feross Aboukhadijeh',
       twitter: 'feross',
+      links: [
+        `[standard](http://standardjs.com)`,
+        `[WebTorrent](https://github.com/feross/webtorrent)`,
+      ],
+      tips: [
+      ],
+      picks: [
+        `[standard-format](https://github.com/maxogden/standard-format) - Formats your code to \`standard\` style`,
+        `[why-is-node-running](https://github.com/mafintosh/why-is-node-running) - Node is running but you don't know why?`,
+        `[available](https://github.com/feross/available) - Find available npm package names – this is how the name “standard” was found!`,
+      ],
     },
     {
       name: 'Jamund Ferguson',
       twitter: 'xjamundx',
+      links: [
+        `[Standard](http://standardjs.com/)`,
+        `[eslint-plugin-promise](https://github.com/xjamundx/eslint-plugin-promise)`,
+        `[ASTExplorer.net](http://astexplorer.net/)`,
+      ],
+      picks: [
+        `[Sawdust & Shavings EP](https://rockyvotolato.bandcamp.com/album/sawdust-shavings-ep)`,
+      ],
     },
   ],
   description: `
@@ -28,32 +65,34 @@ export default {
   `,
   hangoutId: 'cms6et0sffpkf92nucv1nf70c74',
   youTubeId: 'sEXow0FPE3Q',
-  podbeanId: '',
-  host: {},
+  podbeanId: 'dank2-5cc2ff',
+  past: true,
+  host: {
+    links: [
+      `[Webpack Validator](https://github.com/kentcdodds/webpack-validator)`,
+      `[AST Explorer](https://astexplorer.net/)`,
+    ],
+    tips: [
+      `Add \`validate\` script to your npm scripts`,
+    ],
+    picks: [
+      `[GitHub Issue Templates Supported](https://github.com/dear-github/dear-github/issues/125)`,
+      `[npm-run-all](https://www.npmjs.com/package/npm-run-all)`,
+    ],
+  },
   panelists: [
     {
       twitter: 'dan_abramov',
+      picks: [
+        `[Reflections on ESLint success](https://www.nczonline.net/blog/2016/02/reflections-on-eslints-success/)`,
+        `[React Atelier](http://scup.github.io/atellier/material-ui-atellier/)`,
+      ],
     },
     {
       twitter: 'drboolean',
-    },
-    {
-      twitter: 'getify',
-    },
-    {
-      twitter: 'kwuchu',
-    },
-    {
-      twitter: 'linclark',
-    },
-    {
-      twitter: 'mzabriskie',
-    },
-    {
-      twitter: 'pamasaur',
-    },
-    {
-      twitter: 'tylermcginnis33',
+      picks: [
+        `[esprima](http://esprima.org/)`,
+      ],
     },
   ],
 }
