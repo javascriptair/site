ed399b810131957ed7cad694d18f1aeb66ed3988
Kent C. Dodds <kent@doddsfamily.us>
2016-02-19 11:17:30 -0700

    Add autotrack http://googledevelopers.blogspot.com/2016/02/introducing-autotrack-for-analyticsjs.html

--- src/components/scripts/google-analytics.js
+++ src/components/scripts/google-analytics.js
@@ -4,25 +4,21 @@ import React from 'react'
 export default GoogleAnalytics
 
 function GoogleAnalytics() {
-  return <InlineScript fn={runScript} />
+  return (
+    <div>
+      <InlineScript fn={runScript} />
+      <script async src="//www.google-analytics.com/analytics.js"></script>
+      <script async src="https://cdn.rawgit.com/googleanalytics/autotrack/0.5.0/autotrack.js"></script>
+    </div>
+  )
 }
 
 function runScript() {
   /* eslint-disable */
-  (function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function () {
-      (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-      m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
-  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
+  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
   ga('create', 'UA-70331623-1', 'auto');
   ga('require', 'displayfeatures');
+  ga('require', 'autotrack');
   ga('send', 'pageview');
   /* eslint-enable */
 }
