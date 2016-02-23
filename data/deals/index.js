233278a499ffab7cbd99f17e17f21066e7009719
Kent C. Dodds <kent@doddsfamily.us>
2016-02-22 23:52:47 -0800

    Add digital ocean to the list of deals

--- data/deals/index.js
+++ data/deals/index.js
@@ -38,10 +38,25 @@ export default sortBy([
     deal: 'Use code JSAIR for 20% off your ticket',
     expires: '2016-12-05',
     description: `
-    [dotJS](http://dotjs.io) is the largest JavaScript conference in Europe.
-    Its 5th edition will happen on December 5th in Paris at the Théâtre Mogador.
+      [dotJS](http://dotjs.io) is the largest JavaScript conference in Europe.
+      Its 5th edition will happen on December 5th in Paris at the Théâtre Mogador.
       A lot of amazing speakers will be announced soon. Meanwhile you can check the
       dotJS 2015 talks [here](http://www.thedotpost.com/conference/dotjs-2015).
     `,
+  },
+  {
+    imgSrc: '/data/deals/digitalocean.png',
+    organization: 'Digital Ocean',
+    tagline: 'Simple Cloud Infrastructure for Developers',
+    link: 'http://jsair.io/digitalocean',
+    deal: '**JSAIR10** - good for $10 credit on a new DigitalOcean account.',
+    expires: '',
+    description: `
+      DigitalOcean is the best place to get your application off the ground quickly,
+      and the easiest to scale when you find success.
+      Start with the pre-configured NodeJS one-click to get up and running in 55 seconds,
+      or build the exact infrastructure you need, with root access to servers running 100%
+      SSD's in state-of-the-art datacenters around the world.
+    `
   },
 ], i => i.organization.toLowerCase())
