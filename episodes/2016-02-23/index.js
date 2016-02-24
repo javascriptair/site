ba5f2f44ae38acc1578359be3a211cb3550c8bcc
Kent C. Dodds <kent@doddsfamily.us>
2016-02-23 16:08:24 -0800

    Update Live React show #33

--- episodes/2016-02-23/index.js
+++ episodes/2016-02-23/index.js
@@ -8,102 +8,28 @@ export default {
       name: 'React Conf',
       twitter: 'reactjs',
       squareImage: true,
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
     },
   ],
   description: `
     We'll be live at [React Conf](http://conf.reactjs.com/) talking with speakers and others about what's going on at the conference. This is your chance to get an insiders look at the conference if you didn't get a chance to snatch a ticket.
+
+    Guests:
+
+- [Ben Alpert](https://twitter.com/soprano) - React: What Lies Ahead
+- [Andres Suarez](https://twitter.com/zertosh) - Nuclide hacks for React Native development
+- [Erik Schlegel](https://twitter.com/erikschlegel1) and [Eric Rozell](https://twitter.com/ericrozell) - ReactNative UWP
+- [Eric Florenzano](https://twitter.com/ericflo) - React, Meet Virtual Reality
+- [Helena Milosevic](https://twitter.com/SFHelenaSF) - Demystifying Tech Recruiting
+- [Isaac Salier-Hellendag](https://twitter.com/hellendag) - Rich Text Editing with React
+- [Scott Kyle](https://twitter.com/appden) - A New Way to Persist & Query Data in React Native
+- [Jamison Dance](https://twitter.com/jergason) - Rethinking All Practices: Building Applications in Elm
+- [Maria Hollweck](https://twitter.com/maria_hollweck) - React Native: Lessons Learned After Migrating an App from iOS to Android
   `,
   hangoutId: 'c6kdirtpl5i76am6mlhuqis490o',
   youTubeId: 'rgoyT95ce2U',
-  podbeanId: '',
-  host: {
-    links: [
-    ],
-    tips: [
-    ],
-    picks: [
-    ],
-  },
-  panelists: [
-    {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-  ],
+  podbeanId: 'hwwwv-5cf025',
+  past: true,
+  hideShowNotes: true,
 }
 
 
