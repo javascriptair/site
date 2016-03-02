80e3de3cda362c329398d5b3c24266f25a64f04d
Kent C. Dodds <kent@doddsfamily.us>
2016-03-02 12:31:20 -0700

    Add show notes to lodash show #40

--- episodes/2016-03-02/index.js
+++ episodes/2016-03-02/index.js
@@ -1,105 +1,56 @@
 /* eslint max-len:0 */
 
 export default {
-  title: `JavaScript Utils & Lodash`,
+  title: `Lodash & Open Source`,
   guests: [
     {
       name: 'John-David Dalton',
       twitter: 'jdalton',
-      links: [
-      ],
       tips: [
+        `Explore internals of a library/framework`,
       ],
       picks: [
+        `[babel-plugin-lodash](https://github.com/lodash/babel-plugin-lodash)`,
+        `[eslint-plugin-lodash](https://www.npmjs.com/package/eslint-plugin-lodash)`,
+        `[Why Using \`_.chain\` is a Mistake](https://medium.com/making-internets/why-using-chain-is-a-mistake-9bc1f80d51ba#.daxn4v2ll)`,
+        `[First Timers Only](https://medium.com/@kentcdodds/first-timers-only-78281ea47455#.5ubumy5f7)`,
       ],
     },
   ],
   description: `
     [Lodash](https://lodash.com/) v4 had been released (by the time this episode happens). This JavaScript library of utilities is one of the most widely used utility belts of the enormous JavaScript ecosystem. We're going to talk with John, the mastermind behind this utility (and fork of underscore) about his experience building such a ubiquitous tool and what it can do for you.
+    We also delve into how to contribute to open source projects and how to manage them to encourage contribution. Tons of wisdom in this one!
   `,
   hangoutId: 'c143rgrflt08nui9n5h42morjbs',
   youTubeId: 'pqZGVV35BPU',
   podbeanId: '',
   shortUrl: 'http://jsair.io/lodash',
   host: {
-    links: [
-    ],
     tips: [
+      `Do open source`
     ],
     picks: [
+      `[ReactJS Program](http://www.reactjsprogram.com/)`,
+      `[Healthy Open Source](https://medium.com/the-javascript-collection/healthy-open-source-967fa8be7951)`,
+      `[All Contributors](https://github.com/kentcdodds/all-contributors) - ✨ Recognize all contributors, not just the ones who push code ✨`,
     ],
   },
   panelists: [
     {
       twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `[graphviz-preview](https://atom.io/packages/graphviz-preview)`,
+        `[wtf](https://github.com/google/tracing-framework/)`,
+        `[react-makes-you-sad](https://github.com/gaearon/react-makes-you-sad)`,
+        `[Petka Antonov on built-ins](https://github.com/angular/angular.js/issues/6697#issuecomment-42771111)`,
       ],
     },
     {
       twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[Scott Christopher: ContT]( https://twitter.com/_schristo/status/703893702922481664)`,
+        `[Quildreen Motta: No I don’t want to configure your app](http://robotlolita.me/2016/01/09/no-i-dont-want-to-configure-your-app.html)`,
+        `[Purescript online meetup is back]( http://www.meetup.com/Bay-Area-PureScript-Meetup/events/229143832/)`,
       ],
     },
   ],
