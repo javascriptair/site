daac4c8077dc4109d48072ec77c0f88b1d2d3fe5
Kent C. Dodds <kent@doddsfamily.us>
2016-03-07 23:25:36 -0800

    Add shows #15 and #47 to the schedule

---
+++ episodes/2016-03-16/index.js
@@ -0,0 +1,105 @@
+export default {
+  title: `End to End Testing`,
+  guests: [
+    {
+      name: 'Julie Ralph',
+      twitter: 'somejulie',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+  description: `
+    End-to-End testing is among the various forms of testing that is critical for the development and quality assurance of your applications. Join us with a subject matter expert to chat about this important part of application development
+  `,
+  hangoutId: 'c7h4gafbmpo00gc5llo6v737g3o',
+  youTubeId: 'yoPIsrhaNGU',
+  podbeanId: '',
+  host: {
+    links: [
+    ],
+    tips: [
+    ],
+    picks: [
+    ],
+  },
+  panelists: [
+    {
+      twitter: 'dan_abramov',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'drboolean',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'getify',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'kwuchu',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'linclark',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'mzabriskie',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'pamasaur',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'tylermcginnis33',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+}
+
+
---
+++ episodes/2016-03-23/index.js
@@ -0,0 +1,125 @@
+export default {
+  title: `AVA: Futuristic test runner 🚀`,
+  guests: [
+    {
+      name: 'James Talmage',
+      twitter: 'jamestalmage',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      name: 'Mark Wubben',
+      twitter: 'novemberborn',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      name: 'Vadim Demedes',
+      twitter: 'vdemedes',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+  description: `
+    [AVA](http://npm.im/ava) is a relatively new testing framework for JavaScript that promises some really unique and exciting benefits over some of the existing testing frameworks.
+  `,
+  hangoutId: 'cjeb8ka1hu66uapqkrk40j5bjk4',
+  youTubeId: 'YRvrPkXBwdo',
+  podbeanId: '',
+  host: {
+    links: [
+    ],
+    tips: [
+    ],
+    picks: [
+    ],
+  },
+  panelists: [
+    {
+      twitter: 'dan_abramov',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'drboolean',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'getify',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'kwuchu',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'linclark',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'mzabriskie',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'pamasaur',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'tylermcginnis33',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+}
+
+
