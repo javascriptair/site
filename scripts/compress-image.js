f283f16370c4c57226bca8677d829527583eb9b8
Kent C. Dodds <kent@doddsfamily.us>
2016-03-14 14:55:55 -0600

    fix lint issues

--- scripts/compress-image.js
+++ scripts/compress-image.js
@@ -1,46 +1,44 @@
-var inputImagePath = process.argv[2]
-var Imagina = require('imagina')
-var path = require('path')
-var dir = path.dirname(inputImagePath)
-var extension = path.extname(inputImagePath).substring(1)
-var filename = path.basename(inputImagePath, '.' + extension)
-var resizedImagePath = path.join(dir, filename + '.resized.' + extension)
+/* eslint no-console:0 */
+import Imagina from 'imagina'
+import path from 'path'
 
-var im = new Imagina()
+const inputImagePath = process.argv[2]
+const imageDir = path.dirname(inputImagePath)
+const imageExtension = path.extname(inputImagePath).substring(1)
+const imageFilename = path.basename(inputImagePath, '.' + imageExtension)
+const resizedImagePath = path.join(imageDir, imageFilename + '.resized.' + imageExtension)
+
+const im = new Imagina()
 
 resize(function onResizeDone(err) {
   if (err) {
     throw err
   }
   console.log('resize done')
-  if (extension !== 'png') {
-    convert(resizedImagePath, function onConvertDone(err) {
-      if (err) {
-        throw err
+  if (imageExtension !== 'png') {
+    convert(resizedImagePath, function onConvertDone(doneErr) {
+      if (doneErr) {
+        throw doneErr
       }
       console.log('Conversion done')
     })
   } else {
-    console.log('no conversion necessary');
+    console.log('no conversion necessary')
   }
 })
 
 function convert(inputPath, cb) {
-  var dir = path.dirname(inputPath)
-  var extension = path.extname(inputPath).substring(1)
-  var filename = path.basename(inputPath, '.' + extension)
-  var outputPath = path.join(dir, filename + '.png')
+  const dir = path.dirname(inputPath)
+  const extension = path.extname(inputPath).substring(1)
+  const filename = path.basename(inputPath, '.' + extension)
+  const outputPath = path.join(dir, filename + '.png')
   console.log('converting', inputPath, outputPath)
   im.convert(inputPath, outputPath, cb)
 }
 
 function resize(cb) {
-  var params = '-resize 180x180^ -gravity center -extent 180x180'.split(' ')
+  const params = '-resize 180x180^ -gravity center -extent 180x180'.split(' ')
   console.log('resizing', inputImagePath, resizedImagePath)
   im.resize(inputImagePath, resizedImagePath, '180x180', params, cb)
-}
-
-function getFilename(filepath) {
-  return filepath.split(/(\\|\/)/g).pop()
 }
 
