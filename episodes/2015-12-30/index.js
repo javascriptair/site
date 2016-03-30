f69de0af179958337e898179bd108a0be494a646
Kent C. Dodds <kent@doddsfamily.us>
2016-03-30 17:01:14 -0600

    Add shortUrl to functional episode

--- episodes/2015-12-30/index.js
+++ episodes/2015-12-30/index.js
@@ -40,6 +40,7 @@ export default {
   hangoutId: 'c6gcf8f0nirc5t83homjb5trquc',
   youTubeId: '82M9fKe7hiw',
   podbeanId: 'eu658-5b510e',
+  shortUrl: 'http://jsair.io/functional',
   past: true,
   host: {
     tips: ['Try a standing desk'],
