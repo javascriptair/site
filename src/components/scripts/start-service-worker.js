2aaf1c50448bcf58dbbdaecbe91304ae92184081
Kent C. Dodds <kent@doddsfamily.us>
2016-04-07 08:33:10 -0600

    Not sure why, but it appears this broke service-worker everywhere but home

--- src/components/scripts/start-service-worker.js
+++ src/components/scripts/start-service-worker.js
@@ -9,7 +9,7 @@ function StartServiceWorker() {
 function runScript() {
   /* eslint no-console:0 */
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(function onRegister(registration) {
+    navigator.serviceWorker.register('service-worker.js').then(function onRegister(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope)
     }).catch(function onRegistrationFailure(err) {
