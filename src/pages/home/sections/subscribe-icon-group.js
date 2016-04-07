9d5240fe8a7d1e653aff6568a39a6c33e4a56f8e
Kent C. Dodds <kent@doddsfamily.us>
2016-04-06 21:11:26 -0600

    aphrodite-ify subscribe-icon-list

--- src/pages/home/sections/subscribe-icon-group.js
+++ src/pages/home/sections/subscribe-icon-group.js
@@ -1,20 +1,36 @@
+import {StyleSheet, css} from 'aphrodite'
 import IconLink from '../../../components/icon-link'
 
 export default SubscribeIconGroup
 
 function SubscribeIconGroup() {
+  const {styles} = SubscribeIconGroup
   const iconList = [
     {href: 'http://audio.javascriptair.com/feed/', name: 'podcast', title: 'Podcast RSS Feed'},
     {href: 'http://video.javascriptair.com/', name: 'youtube', title: 'YouTube Channel'},
     {href: 'http://itunes.javascriptair.com/', name: 'apple', title: 'iTunes Podcast'},
   ]
   return (
-    <ul className="subscribe-icon-list">
+    <ul className={css(styles.list)}>
       {iconList.map((item, index) => (
-        <li className="subscribe-icon-list__item" key={index}>
+        <li className={css(styles.item)} key={index}>
           <IconLink {...item} />
         </li>
       ))}
     </ul>
   )
 }
+
+SubscribeIconGroup.styles = StyleSheet.create({
+  list: {
+    listStyle: 'none',
+    paddingLeft: 0,
+    margin: 0,
+  },
+  item: {
+    width: 70,
+    height: 60,
+    display: 'inline-block',
+    padding: '0 5px',
+  },
+})
