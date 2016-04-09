c4b802caa473f46ccca59956aab0de2f1f6d98d8
Kent C. Dodds <kent@doddsfamily.us>
2016-04-09 14:59:48 -0600

    aphrodite-ize decor

--- src/components/decor.js
+++ src/components/decor.js
@@ -1,11 +1,28 @@
+import {StyleSheet, css} from 'aphrodite'
 export default Decor
 
 function Decor() {
+  const {styles} = Decor
   return (
-    <div className="decor">
-      <span className="decor__part decor__part--dark"></span>
-      <span className="decor__part decor__part--medium"></span>
-      <span className="decor__part decor__part--light"></span>
+    <div className={css(styles.root)}>
+      <span className={css(styles.part, styles.dark)}></span>
+      <span className={css(styles.part, styles.medium)}></span>
+      <span className={css(styles.part, styles.light)}></span>
     </div>
   )
 }
+
+Decor.styles = StyleSheet.create({
+  root: {
+    backgroundColor: 'tomato',
+    display: 'flex',
+    width: '100%',
+  },
+  part: {
+    height: 15,
+    flexGrow: 1,
+  },
+  dark: {backgroundColor: '#8e832b'},
+  medium: {backgroundColor: '#baab38'},
+  light: {backgroundColor: '#d4c33f'},
+})
