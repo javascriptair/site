0deb4c2c689f7bfd6b4a24a1c4dee24695909ba7
JP Osorio <jpoo90@gmail.com>
2016-04-11 20:10:55 -0400

    Moved twitter functionality to utils.

--- other/add-guest.js
+++ other/add-guest.js
@@ -1,36 +1,21 @@
-/* eslint camelcase:0 */
-import Twitter from 'twitter'
+import {getProfileImageURL} from './utils/twitter'
 import request from 'request'
 import fs from 'fs'
 import path from 'path'
 
 const exec = require('child_process').exec
 
-const TW = new Twitter({
-  consumer_key: process.env.TW_CONSUMER_KEY,
-  consumer_secret: process.env.TW_CONSUMER_SECRET,
-  access_token_key: process.env.TW_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TW_ACCESS_TOKEN_SECRET,
-})
-
 const guestTwitterHandle = process.argv[2]
 const episodeDate = process.argv[3]
 
 const imageName = `${guestTwitterHandle}.png`
 const imgPath = path.join(__dirname, imageName)
-const episodePath = __dirname.replace('other', `episodes/${episodeDate}/${imageName}`)
+const episodePath = path.join(process.cwd(), 'episodes', episodeDate, imageName)
 
-const twitterParams = {screen_name: guestTwitterHandle}
+getProfileImageURL(guestTwitterHandle)
+  .then(downloadImage)
 
-TW.get('users/show.json', twitterParams, parseTWData)
-
-function parseTWData(error, data) {
-  const fullImage = data.profile_image_url.replace('_normal', '')
-
-  getImage(fullImage)
-}
-
-function getImage(imgUrl) {
+function downloadImage(imgUrl) {
   request(imgUrl)
     .pipe(fs.createWriteStream(imgPath).on('close', compressImage))
 }
