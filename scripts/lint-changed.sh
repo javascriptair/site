b1be274f35d58492b0646651ea41c479c4ad54e8
Kent C. Dodds <kent@doddsfamily.us>
2016-04-11 20:37:17 -0600

    add lint-changed

---
+++ scripts/lint-changed.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+# allow being run from somewhere other than the git rootdir
+gitroot=$(git rev-parse --show-cdup)
+# default gitroot to . if we're already at the rootdir
+gitroot=${gitroot:-.};
+nm_bin=$gitroot/node_modules/.bin
+SRC_FILES=$(git diff --staged --diff-filter=ACMTUXB --name-only -- '*.js') && x=1
+if [ $SRC_FILES ]; then
+  echo "Linting changed files 😁"
+  $nm_bin/eslint $SRC_FILES
+  echo "⚡ changed files passed linting! ⚡"
+else
+  echo "No files to lint 🙌"
+fi
