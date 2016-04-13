9fd0252f8fe8bf1da90295ac23435d8bcb77d39e
Kent C. Dodds <kent@doddsfamily.us>
2016-04-13 17:31:21 -0600

    Add links, tips, and picks to the reactive show (#84)
    @mattpodwysocki, if you want to add links to your picks, please feel
    free to make a PR to this :-)

--- episodes/2016-04-13/index.js
+++ episodes/2016-04-13/index.js
@@ -5,30 +5,42 @@ export default {
       name: 'André Staltz',
       twitter: 'andrestaltz',
       links: [
+        `Someone mentioned [the Epic Monadic Promises thread](https://github.com/promises-aplus/promises-spec/issues/94)`,
       ],
       tips: [
       ],
       picks: [
+        `[CycleConf](http://cycleconf.com) and follow the hashtag #cycleconf next week`,
+        `[most](https://github.com/cujojs/most/)`,
       ],
     },
     {
       name: 'Ben Lesh',
       twitter: 'BenLesh',
       links: [
+        `[Paul Taylor](https://github.com/trxcllnt) on github`,
       ],
       tips: [
+        `[Check out the specs on RxJS](https://github.com/ReactiveX/rxjs/tree/master/src/operator)`,
+        `[Check out the docs on how to write marble tests](https://github.com/ReactiveX/rxjs/blob/master/doc/writing-marble-tests.md)`,
       ],
       picks: [
+        `Go to the restaurant that Dan Abramov is at. (Redux Foodtruck?)`,
       ],
     },
     {
       name: 'Matthew Podwysocki',
       twitter: 'mattpodwysocki',
       links: [
+        `[Gleb Bahmutov](https://glebbahmutov.com/)`,
+        `[rxvision](https://github.com/jaredly/rxvision)`,
       ],
       tips: [
       ],
       picks: [
+        `React Native on Windows Phone`,
+        `Node on mobile for P2P interaction`,
+        `[Interactive Extensions for JavaScript](https://github.com/Reactive-Extensions/IxJS)`,
       ],
     },
   ],
@@ -37,87 +49,62 @@ export default {
   `,
   hangoutId: 'cbu3fh4too4a6iot5psgo9cl4d4',
   youTubeId: 'RFvwLifhxDg',
-  podbeanId: '',
+  podbeanId: 'yn6zw-5e65e8',
   shortUrl: 'http://jsair.io/reactive',
+  past: true,
   host: {
-    links: [
-    ],
     tips: [
+      `If you’re feeling overwhelmed, make a Todo list and focus on one task at a time.`,
     ],
     picks: [
+      `[aphrodite](https://github.com/Khan/aphrodite) - It's inline styles, but they work!`,
+      `[autocomplete-emojis](https://atom.io/packages/autocomplete-emojis) Atom package`,
+      `[Stuff](https://github.com/kentcdodds/ama/issues/107) that I use for recording egghead.io lessons`,
+      `Adam. He’s great <3`,
     ],
   },
   panelists: [
     {
       twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `[MobX](https://github.com/mobxjs/mobx)`,
+        `[Optimizing Redux TodoMVC with an optimal state structure](https://github.com/mweststrate/redux-todomvc/pulls?q=is%3Apr+is%3Aclosed)`,
       ],
     },
     {
       twitter: 'drboolean',
       links: [
+        `[https://egghead.io/lessons/rxjs-reactive-programming-what-is-rxjs](egghead rxjs series)`,
+        `[http://highlandjs.org/](highland.js lazy streams)`,
+        `[https://github.com/cujojs/most](most.js reactive stream lib)`,
       ],
       tips: [
+        `Favor laziness since laziness can lead to better performance & extendability`,
+        `Try to compose single expressions which data flows through and, if you struggle, don’t “cheat”, but rather look for stream method alternatives - they are there. It will simplify the whole codebase.`,
+        `Normalization helps composability. I tend to wrap all my utilities in the same type of stream so that my application code works like legos. Wrapping is a pain, but those utilities tend not to change so it’s a one time thing.`,
       ],
       picks: [
+        `[lambdaconf tweetstorm](https://twitter.com/jo_liss/status/719487771967152132)`,
+        `[People vs OJ](http://www.fxnetworks.com/shows/the-people-v-oj-simpson-american-crime-story/episodes)`,
       ],
     },
     {
       twitter: 'getify',
       links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
+        `[RxMarbles.com](http://rxmarbles.com/)`,
+        `[asynquence](http://github.com/getify/asynquence), specifically [asynquence reactive sequences](https://github.com/getify/asynquence/tree/master/contrib#react-plugin)`,
       ],
       picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[A Tale Of Three Lists](https://github.com/getify/a-tale-of-three-lists)`,
       ],
     },
     {
       twitter: 'pamasaur',
       links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
+        `[RxMarbles](http://rxmarbles.com/)`,
       ],
       picks: [
+        `[FantasyLand](https://github.com/fantasyland/fantasy-land)`,
       ],
     },
   ],
