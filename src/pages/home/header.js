0414dc460407a31f168cd48a638ce6c8b602acef
Kent C. Dodds <kent@doddsfamily.us>
2016-04-22 00:54:06 -0700

    aphrodite-ize buttons

--- src/pages/home/header.js
+++ src/pages/home/header.js
@@ -4,6 +4,7 @@ import SubscribeIconGroupSection from './sections/subscribe-icon-group'
 import Icon from '../../components/icon'
 import Decor from '../../components/decor'
 
+const smallScreen = '@media only screen and (max-width : 515px)'
 export default Header
 
 function Heading() {
@@ -26,12 +27,14 @@ function Heading() {
 }
 
 function Hero() {
+  const {styles} = Hero
+  const buttonClassName = css(styles.button)
   return (
     <div className="header__hero">
       <p className="header__tagline">The live broadcast podcast all about JavaScript</p>
-      <div className="btn-container header__btn-container">
-        <a href="#episodes" className="btn btn--large btn--black">Upcoming Shows</a>
-        <a href="#previous-episodes" className="btn btn--large btn--black">Past Shows</a>
+      <div className={css(styles.buttonContainer)}>
+        <a href="#episodes" className={buttonClassName}>Upcoming Shows</a>
+        <a href="#previous-episodes" className={buttonClassName}>Past Shows</a>
       </div>
       <div className="social__container">
         <div className="social social-home">
@@ -49,6 +52,39 @@ function Hero() {
     </div>
   )
 }
+
+Hero.styles = StyleSheet.create({
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: 22,
+    [smallScreen]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  },
+  button: {
+    margin: '0px 10px',
+    display: 'flex',
+    textDecoration: 'none',
+    alignItems: 'center',
+    [smallScreen]: {
+      margin: '5px 0px',
+    },
+    width: 240,
+    fontSize: '1.4em',
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    padding: 10,
+    border: '5px solid #222',
+    color: 'black',
+    transition: '0.4s',
+    ':hover': {
+      backgroundColor: '#222',
+      color: '#efdd4f',
+    },
+  },
+})
 
 function LatestShow({show}) {
   const {styles} = LatestShow
