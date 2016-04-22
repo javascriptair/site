507c405a7db37c7fc147511f3272ed4257cc7e7d
Kent C. Dodds <kent@doddsfamily.us>
2016-04-22 02:26:20 -0700

    aphrodite-ize the episode-small sections

--- src/pages/home/sections/episodes/episode-small.js
+++ src/pages/home/sections/episodes/episode-small.js
@@ -1,10 +1,12 @@
 import {PropTypes} from 'react'
+import {StyleSheet, css} from 'aphrodite'
+import {upToBig} from '../../../../styles/media-queries'
 import {displayListify} from '../../../../../shared/utils'
 
 export default EpisodeSmall
 
 function EpisodeSmall({episodeData}) {
-
+  const {styles} = EpisodeSmall
   const {
     date,
     title,
@@ -14,9 +16,9 @@ function EpisodeSmall({episodeData}) {
   } = episodeData
 
   return (
-    <div className="episode--small">
+    <div className={css(styles.episodeSmall)}>
       <a
-        className="episode--small__title"
+        className={css(styles.title)}
         href={`/episodes/${date}`}
         title={`${title}`}
       >
@@ -24,25 +26,66 @@ function EpisodeSmall({episodeData}) {
         <span dangerouslySetInnerHTML={titleHTML} />
       </a>
 
-      <small className="episode--small__guests">
+      <small className={css(styles.guests)}>
         {' with '}
-        {
-          displayListify(
-            (guests || [])
-              .map((g, gI) => {
-                if (!g.link) {
-                  return <span key={gI}>{g.name}</span>
-                }
-                return <a key={gI} href={g.link}>{g.name}</a>
-              })
-          )
-        }
+        {displayListify(guests.map(mapGuests))}
       </small>
-
     </div>
   )
+
+  function mapGuests(g, gI) {
+    if (!g.link) {
+      return <span key={gI}>{g.name}</span>
+    }
+    return (
+      <a
+        key={gI}
+        href={g.link}
+        className={css(styles.guestsAnchor)}
+      >
+        {g.name}
+      </a>
+    )
+  }
 }
 
 EpisodeSmall.propTypes = {
   episodeData: PropTypes.object,
 }
+
+EpisodeSmall.styles = StyleSheet.create({
+  episodeSmall: {
+    padding: '5px 0px',
+    [upToBig]: {padding: '16px 20px'},
+  },
+  title: {
+    fontSize: '1.3em',
+    color: 'black',
+    textDecoration: 'none',
+    ':hover': {
+      color: '#555',
+    },
+    [upToBig]: {
+      display: 'block',
+      fontSize: '1.6em',
+      textAlign: 'center',
+    },
+  },
+  guests: {
+    fontSize: '0.9em',
+    lineHeight: '1.3',
+    color: 'gray',
+    [upToBig]: {
+      fontSize: '1.2em',
+      paddingTop: 10,
+      display: 'block',
+      textAlign: 'center',
+    },
+  },
+  guestsAnchor: {
+    color: 'gray',
+    ':hover': {
+      color: 'black',
+    },
+  },
+})
