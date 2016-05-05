87845e1fbf4efadd7f06f0d9af75a3009cd019ae
Kent C. Dodds <kent@doddsfamily.us>
2016-05-05 04:40:29 -0600

    Remove duplicate listing for @teropa... 😅 sorry about that!

--- episodes/2016-05-05/index.js
+++ episodes/2016-05-05/index.js
@@ -20,8 +20,7 @@ export default {
     Guests include: [samuel john saccone](https://twitter.com/samccone), [Tero Parviainen](https://twitter.com/teropa),
     [Mike Brocchi](https://twitter.com/brocco), [Jules Kremer](https://twitter.com/jules_kremer),
     [John Papa](https://twitter.com/John_Papa), [Aysegul Yonet](https://twitter.com/AysegulYonet),
-    [Alyssa Nicoll](https://twitter.com/AlyssaNicoll), [Scott Moss](https://twitter.com/scotups),
-    and [Tero Parviainen](https://twitter.com/teropa)
+    [Alyssa Nicoll](https://twitter.com/AlyssaNicoll), and [Scott Moss](https://twitter.com/scotups)
   `,
   hangoutId: 'cbjlovqo30a8qlgnofngrcvoecg',
   youTubeId: 'Q9aL9kmdmIE',
