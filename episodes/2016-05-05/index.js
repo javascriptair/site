0c417551db10b1eb49a366f76efc93c6b49e282b
Kent C. Dodds <kent@doddsfamily.us>
2016-05-06 07:45:23 -0600

    Get ng-conf episode up (#42)

--- episodes/2016-05-05/index.js
+++ episodes/2016-05-05/index.js
@@ -6,12 +6,6 @@ export default {
       name: 'ng-conf',
       twitter: 'ngconf',
       squareImage: true,
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
     },
   ],
   description: `
@@ -22,91 +16,24 @@ export default {
     [John Papa](https://twitter.com/John_Papa), [Aysegul Yonet](https://twitter.com/AysegulYonet),
     [Alyssa Nicoll](https://twitter.com/AlyssaNicoll), [Scott Moss](https://twitter.com/scotups),
     and [Brad Green](https://twitter.com/bradlygreen)
+
+- [samuel john saccone](https://twitter.com/samccone) - Promises are probably older than you.
+- [Tero Parviainen](https://twitter.com/teropa) - Generative Art in Angular 2
+- [Mike Brocchi](https://twitter.com/brocco) - Learn Clingon (Angular CLI)
+- [Scott Moss](https://twitter.com/scotups) - Angular 2 for the rest of us
+- [Aysegul Yonet](https://twitter.com/AysegulYonet) - Pipe Hype (Creating custom pipes)
+- [Alyssa Nicoll](https://twitter.com/AlyssaNicoll) - Wanting it all vs. Being Great
+- [Jules Kremer](https://twitter.com/jules_kremer), [Brad Green](https://twitter.com/bradlygreen), and [Miško Hevery](https://twitter.com/mhevery) - Keynote (Angular Community)
   `,
   hangoutId: 'cbjlovqo30a8qlgnofngrcvoecg',
   youTubeId: 'Q9aL9kmdmIE',
-  podbeanId: '',
+  podbeanId: 'kjqzk-5f14af',
   shortUrl: 'http://jsair.io/ng-conf',
   host: {
     links: [
-    ],
-    tips: [
-    ],
-    picks: [
+      `[The Changlog](https://changelog.com) episode 200: [JavaScript and Robots](https://changelog.com/200) with [Raquel Vélez](https://twitter.com/rockbot)`,
+      `[Healthy Open Source](https://medium.com/the-javascript-collection/healthy-open-source-967fa8be7951) by [Mikeal Rogers](https://twitter.com/mikeal)`,
+      `[Open Source Stamina](https://medium.com/@kentcdodds/open-source-stamina-dafd063f9932) by me 😄`,
     ],
   },
-  panelists: [
-    {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-  ],
 }
