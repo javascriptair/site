d99f5d51ed0eb9e19888fc1bf22c81775cf105a5
Kent C. Dodds <kent@doddsfamily.us>
2016-05-12 09:25:43 -0600

    Add tips, picks, links, and podbeanId to latest show (#74)

--- episodes/2016-05-11/index.js
+++ episodes/2016-05-11/index.js
@@ -4,31 +4,37 @@ export default {
     {
       name: 'Omoju Miller',
       twitter: 'omojumiller',
-      links: [
-      ],
       tips: [
+        `Always ask why?`,
+        `Why do we get the results we have? Why is the sky blue? Never lose your curiosity`,
       ],
       picks: [
+        `[Danger of a single story](https://www.ted.com/talks/chimamanda_adichie_the_danger_of_a_single_story) - TED talk`,
+        `[Tomorrow Looks Bright Newsletter](http://www.tomorrowlooksbright.com/)`,
+        `[Call Your Girlfriend](http://callyourgirlfriend.com/) Podcast`,
+        `[Recode Decode](http://www.recode.net/recode-decode-podcast-kara-swisher)`,
       ],
     },
     {
       name: 'Steve Andrews',
       twitter: 'SteveAndrews',
       links: [
+        `[Jack Ma Says Women Executives Are Alibaba’s ‘Special Sauce’](http://www.huffingtonpost.com/2015/05/20/jack-ma-alibaba-women_n_7344316.html)`,
       ],
       tips: [
-      ],
-      picks: [
+        `[Platinum Bay](http://www.platinumbay.com/) is hiring`,
       ],
     },
     {
       name: 'Kate Edwards',
       twitter: 'IGDA_ED',
       links: [
+        `[Google Unconscious Bias Training](https://www.youtube.com/watch?v=nLjFTHTgEVU)`,
+        `[Facebook Unconscious Bias Training](https://managingbias.fb.com/)`,
+        `[Microsoft Unconscious Bias Training](https://www.mslearning.microsoft.com/course/72169/launch)`,
       ],
       tips: [
-      ],
-      picks: [
+        `Be aware that you - and everyone - has natural biases`,
       ],
     },
   ],
@@ -37,87 +43,39 @@ export default {
   `,
   hangoutId: 'carao3pdrl3bag1kf8l9frerml4',
   youTubeId: '-rH-W483sUg',
-  podbeanId: '',
+  podbeanId: '4ajwx-5f40ab',
   shortUrl: 'http://jsair.io/science',
   host: {
-    links: [
-    ],
     tips: [
+      `Free your mind and use reminders`,
     ],
     picks: [
+      `[You Can Sit With Us](https://cottonbureau.com/products/you-can-sit-with-us) T-Shirt`,
+      `[Erwan Datin](https://twitter.com/erwan_datin) created a JS Air app in React Native! [Repo](https://github.com/MacKentoch/jsair-mobile), [iOS](https://itunes.apple.com/fr/app/js-air/id1112141070?mt=8), [Android](https://play.google.com/store/apps/details?id=com.jsair)`,
+      `[Becoming Betazoid How to Listen and Empathize with Others in the Workplace](https://youtu.be/ypdHBfLXoHI) – [Joe Eames](https://twitter.com/josepheames)`,
     ],
   },
   panelists: [
     {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
       twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `[Algebra Ch 0](http://www.amazon.com/Algebra-Chapter-Graduate-Studies-Mathematics/dp/0821847813)`,
+        `[States and Nomads - Zach Tellman](https://www.youtube.com/watch?v=KGaFcI2UNrI)`,
+        `[PureScript Tropical Miniconf](http://www.meetup.com/Bay-Area-PureScript-Meetup/events/230451975/)`,
       ],
     },
     {
       twitter: 'getify',
       links: [
+        `[A Speaker’s Notes](https://www.youtube.com/watch?v=IwYcVqIjbb4) talk from Fluent Conf ‘16 about making more room on the conference stage for more voices.`,
       ],
       tips: [
+        `Write code as if it was intended for kids to read and learn.`,
       ],
       picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[Simplified JS Jargon](http://jargon.js.org/)`,
+        `[Minimax Algorithm](https://en.wikipedia.org/wiki/Minimax), aka how to have a computer beat/tie you at Tic-Tac-Toe`,
+        `[MakerSquare](http://makersquare.com) opens new NYC campus`,
       ],
     },
   ],
