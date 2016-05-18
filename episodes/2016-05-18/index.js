0752f5adf4f2485063e2f0eeaa3a6e7e5ad2bc40
Kent C. Dodds <kent@doddsfamily.us>
2016-05-18 12:25:53 -0600

    Add links, tips, and picks for GraphQL show #21

--- episodes/2016-05-18/index.js
+++ episodes/2016-05-18/index.js
@@ -5,20 +5,30 @@ export default {
       name: 'Lee Byron',
       twitter: 'leeb',
       links: [
-      ],
-      tips: [
+        `[Awesome GraphQL](https://github.com/chentsulin/awesome-graphql)`,
+        `[Lokka](https://github.com/kadirahq/lokka)`,
       ],
       picks: [
+        `[Suspension, a novel](http://readsuspension.com/)`,
       ],
     },
     {
       name: 'Steven Luscher',
       twitter: 'steveluscher',
       links: [
+        `Zero to GraphQL on GitHub (https://github.com/steveluscher/zero-to-graphql)`,
+        `Zero to GraphQL Video (https://www.youtube.com/watch?v=UBGzsb2UkeY)`,
+        `Wrapping a REST API in GraphQL blog post (http://graphql.org/blog/rest-api-graphql-wrapper/)`,
+        `[Charles](https://www.charlesproxy.com/)`,
+        `[Apollo](https://github.com/apollostack/apollo)`,
+        `[postgraphql](https://github.com/calebmer/postgraphql)`,
       ],
       tips: [
+        `When you arrive at an n-way stop at the same time as one or more vehicles, who should go first? Take your right hand, and point it to the right of you. If you’re pointing at nobody, it’s your turn to go!`,
       ],
       picks: [
+        `Music: [Spirits – The Strumbellas](https://www.youtube.com/watch?v=F9kXstb9FF4)`,
+        `Book: [The Lost Chord – Conrad Amenta](http://www.lulu.com/shop/conrad-amenta/the-lost-chord/paperback/product-21449175.html)`,
       ],
     },
   ],
@@ -33,81 +43,26 @@ export default {
     links: [
     ],
     tips: [
+      `Colocate **AMAR (as much as reasonable)**`,
     ],
     picks: [
+      `[LGTM](https://lgtm.co)`,
+      `[Code Cartoons](https://code-cartoons.com) - [A cartoon guide to Facebook’s Relay](https://code-cartoons.com/a-cartoon-intro-to-facebook-s-relay-part-1-3ec1a127bca5)`,
+      `[Aphrodite](https://npmjs.com/package/aphrodite) - CSS in JS done right`,
     ],
   },
   panelists: [
     {
       twitter: 'dan_abramov',
-      links: [
-      ],
       tips: [
+        `[Colocate selectors with reducers](https://github.com/reactjs/redux/blob/eeaa4cf7711f5c5ea9789ad90bd5e8fee1f0e62e/examples/shopping-cart/reducers/index.js#L10-L37)`,
       ],
       picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[Reason](http://facebook.github.io/reason/)`,
+        `[React Tiny Renderer](https://github.com/iamdustan/tiny-react-renderer)`,
+        `[Learn Redux](https://learnredux.com/)`,
+        `[Mark Erikson’s link collections](https://github.com/markerikson)`,
+        `[Apollo Client](http://docs.apollostack.com/apollo-client/)`,
       ],
     },
   ],
