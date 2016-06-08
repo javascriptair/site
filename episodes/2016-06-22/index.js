235f0953e32b42ef0a33b3c1b75cdc95a97d6eb5
Kent C. Dodds <kent@doddsfamily.us>
2016-06-08 14:28:39 -0600

    Add some more shows

---
+++ episodes/2016-06-22/index.js
@@ -0,0 +1,98 @@
+export default {
+  title: `On site at KCDC`,
+  guests: [
+    {
+      name: 'KCDC',
+      twitter: 'kc_dc',
+    },
+  ],
+  description: `
+    We'll be live and on site at the Kansas City Developer Conference! This conference covers all aspects of software development and we're sure to have really interesting conversations with a lot of people!
+  `,
+  hangoutId: 'c026gdo852o7ml0bn0mu5at0f70',
+  youTubeId: 'ANTWWoKrwGE',
+  podbeanId: '',
+  shortUrl: 'http://jsair.io/kcdc',
+  host: {
+    links: [
+    ],
+    tips: [
+    ],
+    picks: [
+    ],
+  },
+  panelists: [
+    {
+      twitter: 'dan_abramov',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'drboolean',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'getify',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'kwuchu',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'linclark',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'mzabriskie',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'pamasaur',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'tylermcginnis33',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+}
---
+++ episodes/2016-06-29/index.js
@@ -0,0 +1,104 @@
+export default {
+  title: `Web Animations`,
+  guests: [
+    {
+      name: 'Matias Niemelä',
+      twitter: 'yearofmoo',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+  description: `
+    It&#x27;s totally mind blowing what can be done to enhance the user&#x27;s experience with animations on the web these days. Let&#x27;s talk about how some of this is done and what tools are out there to help you with this.
+  `,
+  hangoutId: 'cf37rj3l0b03ir4od5t15jbsfrs',
+  youTubeId: '5JpsSC_gumk',
+  podbeanId: '',
+  shortUrl: 'http://jsair.io/animations',
+  host: {
+    links: [
+    ],
+    tips: [
+    ],
+    picks: [
+    ],
+  },
+  panelists: [
+    {
+      twitter: 'dan_abramov',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'drboolean',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'getify',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'kwuchu',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'linclark',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'mzabriskie',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'pamasaur',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'tylermcginnis33',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+}
