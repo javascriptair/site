fe83c9c2944f5bcd04efc8e5891237ce44c402f4
Kent C. Dodds <kent@doddsfamily.us>
2016-06-08 14:08:54 -0600

    Add a TBA episode

---
+++ episodes/2016-06-15/index.js
@@ -0,0 +1,92 @@
+export default {
+  title: `TBA`,
+  description: `
+    This show is yet to be nailed down, but you can bet it'll be awesome!
+  `,
+  hangoutId: 'ck3nvcs9ahbtpkj172hihdrt6mo',
+  youTubeId: '1glcyvhf5yY',
+  podbeanId: '',
+  shortUrl: 'http://jsair.io/2016-06-15',
+  host: {
+    links: [
+    ],
+    tips: [
+    ],
+    picks: [
+    ],
+  },
+  panelists: [
+    {
+      twitter: 'dan_abramov',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'drboolean',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'getify',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'kwuchu',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'linclark',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'mzabriskie',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'pamasaur',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'tylermcginnis33',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+}
