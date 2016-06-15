e1c22239ff4f5e317e96c30bfa699f46f26df7ec
Kent C. Dodds <kent@doddsfamily.us>
2016-06-15 14:24:53 -0600

    Add links, tips, and picks and podbeanId to For the beginners (#109)

--- episodes/2016-06-15/index.js
+++ episodes/2016-06-15/index.js
@@ -4,41 +4,52 @@ export default {
     {
       name: 'Aimee Knight',
       twitter: 'aimee_knight',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `[You Don’t Know JS](https://github.com/getify/You-Dont-Know-JS)`,
+        `[Operation Code](https://operationcode.org/)`,
+        `[Newbie Remote Conf](https://allremoteconfs.com/newbie-2016)`,
+        `[Grokking Algorithms](https://www.manning.com/books/grokking-algorithms)`,
       ],
     },
     {
       name: 'Tim Dorr',
       twitter: 'timdorr',
       links: [
+        `[LOGO programming](https://www.youtube.com/watch?v=mttyoaspa7U)`,
+        `[Unreal Engine](https://www.unrealengine.com/what-is-unreal-engine-4)`,
       ],
       tips: [
+        `Contribute to project [docs](https://github.com/reactjs/react-router/issues/2933)!`,
       ],
       picks: [
+        `[Practical Object-Oriented Design in Ruby (POODR)](http://www.poodr.com/)`,
+        `[Webpack Preset](https://github.com/webpack-preset/webpack-preset)`,
+        `[Lady Dynamite on Netflix](https://www.netflix.com/watch/80046194)`,
+        `[Keto (ketosis/ketogenic) diet](https://www.reddit.com/r/keto)`,
       ],
     },
     {
       name: 'Max Stoiber',
       twitter: 'mxstbr',
       links: [
+        `[Carte Blanche](https://github.com/carteb/carte-blanche)`,
       ],
       tips: [
+        `You don't have to be Dan or Ryan to get a job`,
       ],
       picks: [
+        `[DLLPlugin](https://medium.com/@soederpop/webpack-plugins-been-we-been-keepin-on-the-dll-cdfdd6cb8cd7)`,
+        `[LookUp app](http://www.96problems.com/look-up/)`,
       ],
     },
     {
       name: 'Scott Moss',
       twitter: 'scotups',
-      links: [
-      ],
       tips: [
+        `Try to use npm scripts directly for your next project and not a build tool like gulp`,
       ],
       picks: [
+        `[Overwatch](https://playoverwatch.com/)`,
       ],
     },
   ],
@@ -47,87 +58,73 @@ export default {
   `,
   hangoutId: 'ck3nvcs9ahbtpkj172hihdrt6mo',
   youTubeId: '1glcyvhf5yY',
-  podbeanId: '',
+  podbeanId: 'zn6r9-603346',
   shortUrl: 'http://jsair.io/beginners',
   host: {
     links: [
+      `[JavaScript and Robots](http://5by5.tv/changelog/200) with [Raquel Vélez](https://twitter.com/rockbot)`,
     ],
     tips: [
+      `Learn one new thing every day (#eggheadADay)`,
+      `The learning workflow: Consume, Build, Teach`,
     ],
     picks: [
+      `[react-html-email](https://www.npmjs.com/package/react-html-email) `,
+      `[aphrodite](https://npmjs.com/package/aphrodite)`,
     ],
   },
   panelists: [
     {
       twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `[A re-introduction to JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript)`,
+        `[Understanding ES6](https://leanpub.com/understandinges6)`,
+        `[Teach Yourself Programming in 10 Years](http://norvig.com/21-days.html)`,
+        `[On Spectrum of Abstraction](https://www.youtube.com/watch?v=mVVNJKv9esE)`,
+        `[Making JavaScript Initialize Faster](https://www.youtube.com/watch?v=xbZzahWakGs)`,
+        `[HtmlWebpackPlugin](https://github.com/ampedandwired/html-webpack-plugin)`,
+        `[Babel Plugin Handbook](https://github.com/thejameskyle/babel-handbook/blob/master/translations/en/plugin-handbook.md)`,
       ],
     },
     {
       twitter: 'drboolean',
-      links: [
-      ],
       tips: [
+        `Learn the language, learn the paradigms, then learn the libraries/frameworks`,
+        `es5 is still huge. Perhaps start there and learn es2015 after.`,
       ],
       picks: [
+        `[Orlando Shooting GoFundMe](https://www.gofundme.com/29bubytq)`,
+        `[YOW Lambda Jam 2016 talks](https://www.youtube.com/channel/UCAvGvvEemkeX8hurdPXr7hA)`,
+        `[Functional JavaScript](https://www.manning.com/books/functional-programming-in-javascript)`,
       ],
     },
     {
       twitter: 'getify',
       links: [
+        `Software Engineering Daily podcast, [episode with Kyle Simpson](http://softwareengineeringdaily.com/2016/06/12/2610/)`,
       ],
       tips: [
+        `Everyone: take a walk every single day, and replace one drink a day with water`,
+        `Beginner Devs: write your code as simply and verbosely as possible, to explain your thinking (or lack of it!) clearly. Don’t just emulate the clever code you see from others.`,
       ],
       picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[SimpleHTTP2Server](https://github.com/GoogleChrome/simplehttp2server) and [demo](https://www.youtube.com/watch?v=qx9tHwhjkHs)`,
+        `[Brotli](https://samsaffron.com/archive/2016/06/15/the-current-state-of-brotli-compression) compression`,
+        `[You Don’t Know JS](http://youdontknowjs.com), specifically [Up & Going](https://github.com/getify/You-Dont-Know-JS/tree/master/up%20%26%20going#you-dont-know-js-up--going)`,
       ],
     },
     {
       twitter: 'pamasaur',
       links: [
+        `[The Mess We’re In (talk by Joe Armstrong)](https://www.youtube.com/watch?v=lKXe3HUG2l4)`,
       ],
       tips: [
+        `Beginners: Don’t stay stuck for more than 15 minutes. Ask for help!`,
       ],
       picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[Nand2Tetris](http://www.nand2tetris.org/)`,
+        `[Nand2Tetris on Coursera](https://www.coursera.org/learn/build-a-computer)`,
+        `[Beyond the Resume: How to Get Your Next Job as a Developer](https://gumroad.com/l/beyondtheresume/)`,
       ],
     },
   ],
