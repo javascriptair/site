33fcd62eb1e41971f4c67ca11da037717d3efdfa
Kent C. Dodds <kent@doddsfamily.us>
2016-06-17 13:46:58 -0600

    Going to embed the YouTube video in it instead

--- src/pages/episode/index.js
+++ src/pages/episode/index.js
@@ -146,9 +146,11 @@ FutureEpisodeStuff.styles = StyleSheet.create({
 
 function getHeadTags(episode) {
   /* eslint react/jsx-max-props-per-line:0, max-len:0 */
-  const {metaDescription, page, taglessTitle, date} = episode
+  const {metaDescription, page, taglessTitle, date, youTubeId} = episode
   const episodeUrl = `https://javascriptair.com${page}`
   const image = `${episodeUrl}/screenshot.png`
+  const youTubeEmbed = `https://www.youtube.com/embed/${youTubeId}`
+  const youTubeUrl = `https://www.youtube.com/v/${youTubeId}`
   return [
     // Google
     <meta key="g1" name="description" content={metaDescription} />,
@@ -158,20 +160,33 @@ function getHeadTags(episode) {
     <meta key="g5" name="application-name" content="JavaScript Air Podcast" />,
     // Facebook
     <meta key="f1" property="og:title" content={taglessTitle} />,
-    <meta key="f2" property="og:type" content="video.episode" />,
+    <meta key="f2" property="og:type" content="video" />,
     <meta key="f3" property="og:image" content={image} />,
     <meta key="f4" property="og:url" content={episodeUrl} />,
     <meta key="f5" property="og:description" content={metaDescription} />,
     <meta key="f6" property="og:site_name" content="JavaScript Air" />,
     <meta key="f7" property="video:release_date" content={date} />,
+    <meta key="f8" property="og:video:url" content={youTubeEmbed} />,
+    <meta key="f9" property="og:video:secure_url" content={youTubeEmbed} />,
+    <meta key="f10" property="og:video:type" content="text/html" />,
+    <meta key="f11" property="og:video:width" content="1280" />,
+    <meta key="f12" property="og:video:height" content="720" />,
+    <meta key="f13" property="og:video:url" content={`${youTubeUrl}?version=3&amp;autohide=1`} />,
+    <meta key="f14" property="og:video:secure_url" content={`${youTubeUrl}?version=3&amp;autohide=1`} />,
+    <meta key="f15" property="og:video:type" content="application/x-shockwave-flash" />,
+    <meta key="f16" property="og:video:width" content="1280" />,
+    <meta key="f17" property="og:video:height" content="720" />,
     // Twitter
-    <meta key="t1" name="twitter:card" content="summary_large_image" />,
+    <meta key="t1" name="twitter:card" content="player" />,
     <meta key="t2" name="twitter:title" content={taglessTitle} />,
     <meta key="t3" name="twitter:description" content={metaDescription} />,
     <meta key="t4" name="twitter:image" content={image} />,
     <meta key="t5" name="twitter:image:alt" content="JavaScript Air episode card with information about the guests, the episode description, and the episode date and time" />,
     <meta key="t6" name="twitter:site" content="@JavaScriptAir" />,
     <meta key="t7" name="twitter:creator" content="@JavaScriptAir" />,
+    <meta key="t8" name="twitter:player" content={youTubeEmbed} />,
+    <meta key="t9" name="twitter:player:width" content="1280"/>,
+    <meta key="t10" name="twitter:player:height" content="720"/>,
   ]
 }
 
