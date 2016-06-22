2b6aed9b2423c56648ea3d226ac09d4163033a62
hdjirdeh <h.djirdeh@mail.utoronto.ca>
2016-06-22 19:17:52 -0400

    Removed legacy css file, finished migrating singleton styles

--- src/pages/episode/sections/show-notes/index.js
+++ src/pages/episode/sections/show-notes/index.js
@@ -1,4 +1,6 @@
 import {PropTypes} from 'react'
+import {StyleSheet, css} from 'aphrodite'
+
 import PersonNotes from './person-notes'
 import {sortPeople, intersperse} from '<utils>/utils'
 
@@ -26,12 +28,13 @@ ShowNotes.propTypes = {
 }
 
 function PeopleNotes({people = []}) {
+  const {styles} = PeopleNotes
   return (
     <div>
       {
         intersperse(people.map((person, index) => (
           <PersonNotes person={person} key={index} />
-        )), (p, i) => <hr key={`hr${i}`} className="person-note-separator" />)
+        )), (p, i) => <hr key={`hr${i}`} className={css(styles.noteSeparator)} />)
       }
     </div>
   )
@@ -40,3 +43,17 @@ function PeopleNotes({people = []}) {
 PeopleNotes.propTypes = {
   people: PropTypes.array,
 }
+
+PeopleNotes.styles = StyleSheet.create({
+  noteSeparator: {
+    width: '90%',
+    '@media only screen and (max-width: 767px)': {
+      marginTop: 20,
+      marginBottom: 20,
+    },
+    '@media only screen and (min-width: 768px)': {
+      marginTop: 36,
+      marginBottom: 36,
+    },
+  },
+})
