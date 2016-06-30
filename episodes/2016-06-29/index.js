524a2480e380886e0c981d4657827a108a6aaa0c
Kent C. Dodds <kent@doddsfamily.us>
2016-06-29 20:33:04 -0600

    Add links, tips, and picks to Web animations (#106)

--- episodes/2016-06-29/index.js
+++ episodes/2016-06-29/index.js
@@ -5,30 +5,53 @@ export default {
       name: 'Matias Niemelä',
       twitter: 'yearofmoo',
       links: [
+        `[Element Animate Polyfill](https://github.com/angular/element-animate-polyfill)`,
       ],
       tips: [
+        `Keep up with the Angular RC releases`,
       ],
       picks: [
+        `[Angular Connect](http://angularconnect.com/)`,
+        `[Web unleashed](http://fitc.ca/event/webu16/)`,
       ],
     },
     {
       name: 'Rachel Nabors',
       twitter: 'rachelnabors',
       links: [
+        `[WebAnimationWeekly.com](http://WebAnimationWeekly.com)`,
+        `[slack.animationatwork.com](http://slack.animationatwork.com)`,
+        `[DevTools Challenger](http://devtoolschallenger.com/)`,
+        `[A List Apart Article on using animation to improve your users’ experience](http://alistapart.com/article/web-animation-at-work)`,
+        `[Complex timing function proposal](https://github.com/w3c/csswg-drafts/issues/229)`,
       ],
       tips: [
+        `Steady FPS over peaks and valleys`,
+        `[Complete Web Animations API documentation](https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API)`,
+        `[Detailed browser support for the Web Animations API at the Edge Platform API catalog--don’t trust caniuse on this beast of an API!](https://developer.microsoft.com/en-us/microsoft-edge/platform/catalog/spec/web-animations-1/)`,
       ],
       picks: [
+        `[My new CSS Animations + Transitions Course](courses.rachelnabors.com)`,
+        `[CSS Conf Argentina](http://cssconfar.com/)`,
+        `[CSS Dev Conf San Antonio](http://2016.cssdevconf.com/) plus [CSS + Motion Design workshop](http://2016.cssdevconf.com/#workshops)`,
+        `[All Things Open](http://allthingsopen.org/)`,
       ],
     },
     {
       name: 'Sarah Drasner',
       twitter: 'sarah_edo',
       links: [
+        `[My CodePen](http://codepen.io/sdras/)`,
+        `[Comparison of Animation Technologies on CSS-Tricks](https://css-tricks.com/comparison-animation-technologies/)`,
+        `[Frontend Masters Course](https://frontendmasters.com/courses/svg-animation/)`,
+        `O’Reilly book coming: SVG Animations`,
+        `[The First Pull Request](http://youtu.be/HjgZQeMrw6c) and [Zero to 60 in Software Development: How to Jumpstart Your Career](https://youtu.be/-qPh6I2hfjw) - talks by [Kent C. Dodds](https://twitter.com/kentcdodds)`,
       ],
       tips: [
+        `[Greensock SVG Tips](http://greensock.com/svg-tips)`,
       ],
       picks: [
+        `[Recreating the Twitter Heart Animation](https://css-tricks.com/recreating-the-twitter-heart-animation/)`,
       ],
     },
   ],
@@ -37,87 +60,31 @@ export default {
   `,
   hangoutId: 'cf37rj3l0b03ir4od5t15jbsfrs',
   youTubeId: '5JpsSC_gumk',
-  podbeanId: '',
+  podbeanId: 'r3axn-6096c8',
   shortUrl: 'http://jsair.io/animations',
   host: {
-    links: [
-    ],
     tips: [
+      `Write a letter to someone`,
     ],
     picks: [
+      `[React Rally](http://www.reactrally.com/)`,
+      `My [Frontend Masters](https://frontendmasters.com) workshops. [Webpack Deep Dive](http://kcd.im/fem-webpack) and [Writing an Open Source Library](http://kcd.im/fem/oss)`,
+      `My [Webpack Playlist](http://kcd.im/egghead-webpack) on Egghead.io (includes my [Webpack course](http://kcd.im/webpack-course))`,
     ],
   },
   panelists: [
     {
       twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[Immutability is not enough](https://codewords.recurse.com/issues/six/immutability-is-not-enough)`,
+        `[A Deepdive into Flow](https://www.youtube.com/watch?v=VEaDsKyDxkY)`,
       ],
     },
     {
       twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `[What I learned from analysing 1.65M versions of Node.js modules in NPM](https://blog.nodeswat.com/what-i-learned-from-analysing-1-65m-versions-of-node-js-modules-in-npm-a0299a614318#.awfyildrp)`,
+        `[React.js Program](http://www.reactjsprogram.com/)`,
       ],
     },
   ],
