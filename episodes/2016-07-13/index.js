422761bd4801d9ceef0e5069b6f86b7564038fc3
Kent C. Dodds <kent@doddsfamily.us>
2016-07-13 19:27:04 -0600

    update a11y show (#108)

--- episodes/2016-07-13/index.js
+++ episodes/2016-07-13/index.js
@@ -4,31 +4,34 @@ export default {
     {
       name: 'Dylan Barrell',
       twitter: 'dylanbarrell',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `[Deque University](https://dequeuniversity.com/) `,
+        `[React](https://facebook.github.io/react/) & [Redux](http://redux.js.org/)`,
+        `[React Axe](https://github.com/dylanb/react-axe) - [React A11y](https://github.com/reactjs/react-a11y)`,
+        `[Crew Seekers](https://www.crewseekers.net/)`,
       ],
     },
     {
       name: 'Marcy Sutton',
       twitter: 'marcysutton',
       links: [
+        `[axe-core](https://www.npmjs.com/package/axe-core) node module`,
       ],
       tips: [
+        `Get away from the computer and GO OUTSIDE.`,
       ],
       picks: [
+        `I'm speaking about aXe at [npm camp](http://npm.camp/)`,
+        `I'm speaking at [React Rally](http://www.reactrally.com/)`,
+        `Installing the [Chrome Accessibility Inspector](https://gist.github.com/marcysutton/0a42f815878c159517a55e6652e3b23a)`,
       ],
     },
     {
       name: 'Wilco Fiers',
       twitter: 'wilcofiers',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `[The WAI Perspectives](https://www.w3.org/WAI/perspectives/)`,
+        `Looking for help developing new rules for a11y testing`,
       ],
     },
   ],
@@ -37,87 +40,30 @@ export default {
   `,
   hangoutId: 'c86bqmgr1bk53qec2b4qqfui8u0',
   youTubeId: 'KSUBrO48k5M',
-  podbeanId: '',
+  podbeanId: 'a6g53-60f165',
   shortUrl: 'http://jsair.io/auto-a11y',
   host: {
-    links: [
-    ],
     tips: [
+      `Don’t spend money on Pokemon Go 😀`,
     ],
     picks: [
+      `[Contributing to React JS](https://www.youtube.com/watch?v=wUpPsEcGsg8)`,
+      `[eslint-plugin-jsx-a11y](https://www.npmjs.com/package/eslint-plugin-jsx-a11y)`,
     ],
   },
   panelists: [
     {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
       twitter: 'drboolean',
       links: [
+        `[Clarity Conf: Baking Accessibility In](http://bradfrost.com/blog/post/clarity-conf-baking-accessibility-in/) by [Brad Frost](https://twitter.com/brad_frost)`,
       ],
       tips: [
+        `Get familiar with your screenreader.`,
       ],
       picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[Gershom Bazerman: Representing Applicatives](http://comonad.com/reader/2013/representing-applicatives/)`,
+        `[Alissa Pajer: Category Theory: An Abstraction for Anything](https://www.infoq.com/presentations/category-theory)`,
+        `[Practically Functional Programming Workshop](http://forwardjs.com#practically-functional-programming)`,
       ],
     },
   ],
--- other/components/show-description.js
+++ other/components/show-description.js
@@ -3,29 +3,28 @@ import Markdown from './markdown'
 export default ShowDescription
 
 function ShowDescription() {
+  /* eslint react/no-comment-textnodes:0 */ // because it's fine here
   return (
     <Markdown>
-      {`
-        [JavaScriptAir](http://javascriptair.com) is the live broadcast podcast
-        all about JavaScript hosted by
-        [egghead.io instructor](https://egghead.io/instructors/kentcdodds)
-        [Kent C. Dodds](https://twitter.com/kentcdodds).
-        Please visit the JavaScript Air website
-        ([javascriptair.com](http://javascriptair.com))
-        to see upcoming and past episodes.
-        Go to [jsair.io/suggest](http://jsair.io/suggest) to
-        suggest topics and guests for the show.
-        Go to [jsair.io/feedback](http://jsair.io/feedback) to
-        provide feedback on this and other episodes.
-        Also be sure to subscribe to our email newsletter
-        at [jsair.io/email](http://jsair.io/email) and
-        follow JavaScript Air on
-        [Twitter](https://twitter.com/JavaScriptAir)
-        and [Google+](https://plus.google.com/105493143005968326308)
-        to stay up to date with future episodes.
-        Also, all episodes are on the
-        [YouTube](http://video.javascriptair.com) channel as well.
-      `}
+      [JavaScriptAir](http://javascriptair.com) is the live broadcast podcast
+      all about JavaScript hosted by
+      [egghead.io instructor](https://egghead.io/instructors/kentcdodds)
+      [Kent C. Dodds](https://twitter.com/kentcdodds).
+      Please visit the JavaScript Air website
+      ([javascriptair.com](http://javascriptair.com))
+      to see upcoming and past episodes.
+      Go to [jsair.io/suggest](http://jsair.io/suggest) to
+      suggest topics and guests for the show.
+      Go to [jsair.io/feedback](http://jsair.io/feedback) to
+      provide feedback on this and other episodes.
+      Also be sure to subscribe to our email newsletter
+      at [jsair.io/email](http://jsair.io/email) and
+      follow JavaScript Air on
+      [Twitter](https://twitter.com/JavaScriptAir)
+      and [Google+](https://plus.google.com/105493143005968326308)
+      to stay up to date with future episodes.
+      Also, all episodes are on the
+      [YouTube](http://video.javascriptair.com) channel as well.
     </Markdown>
   )
 }
