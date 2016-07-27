2d5cbc76c88c4173178fce710380bcafeb32865c
Kent C. Dodds <kent@doddsfamily.us>
2016-07-27 13:05:24 -0600

    Add links, tips, and picks to Web Audio show (#111)

--- episodes/2016-07-27/index.js
+++ episodes/2016-07-27/index.js
@@ -5,30 +5,32 @@ export default {
       name: 'Chris Lowis',
       twitter: 'chrislowis',
       links: [
+        `[Web Audio Weekly](http://chrislowis.co.uk/waw.html)`,
+        `[Web Audio API @ MDN](https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API)`,
+        `[Web Audio Slack Channel](https://web-audio-slackin.herokuapp.com/)`,
       ],
       tips: [
+        `[Canopy](http://hoch.github.io/canopy/) is a Web Audio scratch pad / visualisation / debugging tool`,
+        `[Web Audio Dev Tools in Firefox](https://hacks.mozilla.org/2014/06/introducing-the-web-audio-editor-in-firefox-developer-tools/)`,
+        `[Web Audio School](http://mmckegg.github.io/web-audio-school/)`,
       ],
       picks: [
+        `[Lost Art Press](https://lostartpress.com/)`,
       ],
     },
     {
       name: 'Alejandro Mantecon Guillen',
       twitter: 'alemangui',
       links: [
+        `[Recreating the BBC's sounds of the radiophonic workshop](http://webaudio.prototyping.bbc.co.uk/)`,
+        `[Pizzicato library](https://github.com/alemangui/pizzicato)`,
+        `[Web audio resources](https://github.com/alemangui/web-audio-resources)`,
       ],
       tips: [
+        `When you dive into new realms of programming, be it web-audio, functional programming, or whatever it is you're after, don't be afraid to dive in and iterate. Start with simple micro-projects and keep growing a notch every time. I find it hard to really get into new technology without getting my hands dirty from the start. Practice is essential.`,
       ],
       picks: [
-      ],
-    },
-    {
-      name: 'Chris Coniglio',
-      twitter: 'tencircles',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[Chrome's musiclab](https://musiclab.chromeexperiments.com/)`,
       ],
     },
   ],
@@ -37,88 +39,15 @@ export default {
   `,
   hangoutId: 'cmk6drjd25odn32q83ke59elssk',
   youTubeId: 'sucLfBy-dVY',
-  podbeanId: '',
+  podbeanId: 'beruc-614d9c',
   shortUrl: 'http://jsair.io/audio',
   host: {
-    links: [
-    ],
     tips: [
+      `Make a chrome extension to make the tweet sound effect I was talking about :)`,
+      `Check out [Tero Parviainen](https://twitter.com/teropa)’s talk at [ng-europe](https://ngeurope.org/#speakers) and at [ng-conf](https://www.youtube.com/watch?v=vsl5O4ps7LE)`,
     ],
     picks: [
+      `[p-s](https://npmjs.com/package/p-s) All the benefits of npm scripts without the cost of a bloated package.json and limits of json`,
     ],
   },
-  panelists: [
-    {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-  ],
 }
