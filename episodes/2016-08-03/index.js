2fb7b73dbbdd397c602699ac30f476541a12b76f
Kent C. Dodds <kent@doddsfamily.us>
2016-08-03 13:41:13 -0600

    add links, tips, picks, and podbeanId to Typed FP (#114)

--- episodes/2016-08-03/index.js
+++ episodes/2016-08-03/index.js
@@ -4,41 +4,49 @@ export default {
     {
       name: 'Alfonso García-Caro',
       twitter: 'alfonsogcnunez',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `[Fable](http://fsprojects.github.io/Fable/)`,
+        `[F# for fun and profit](http://fsharpforfunandprofit.com/) - Learning F# and FP`,
+        `[Ionide](http://ionide.io/) - F# plugin for Atom and Visual Studio Code`,
       ],
     },
     {
       name: 'Richard Feldman',
       twitter: 'rtfeldman',
       links: [
+        `[How to use Elm at Work](http://elm-lang.org/blog/how-to-use-elm-at-work)`,
+        `[NoRedInk](https://www.noredink.com/jobs)`,
       ],
       tips: [
+        `Don’t wait for a rewrite! Introduce a new language incrementally. Build a small thing that works. Get that small thing in production. Then expand.`,
       ],
       picks: [
+        `[Elm in Action](https://manning.com/books/elm-in-action)`,
+        `[Frontend Masters 2-day Elm Workshop](https://frontendmasters.com/workshops/elm/)`,
+        `[6 months of Elm in Production](https://www.youtube.com/watch?v=R2FtMbb-nLs)`,
+        `[Building a Live-Validated Signup Form in Elm](http://tech.noredink.com/post/129641182738/building-a-live-validated-signup-form-in-elm)`,
       ],
     },
     {
       name: 'Phil Freeman',
       twitter: 'paf31',
       links: [
-      ],
-      tips: [
+        `[PureScript project website](http://purescript.org)`,
       ],
       picks: [
+        `[Pux library](https://github.com/alexmingoia/purescript-pux)`,
       ],
     },
     {
       name: 'Jordan Walke',
       twitter: 'jordwalke',
       links: [
-      ],
-      tips: [
+        `[Flow](https://flowtype.org/)`,
+        `[ML](https://en.wikipedia.org/wiki/ML_(programming_language))`,
       ],
       picks: [
+        `Vim-mode-plus: Atom plugin recreating Vim’s editing paradigm https://github.com/t9md/atom-vim-mode-plus`,
+        `Real World OCaml: Great technical book, free online version: https://realworldocaml.org/`,
       ],
     },
   ],
@@ -47,87 +55,36 @@ export default {
   `,
   hangoutId: 'ca87vqg4bsqfj63f7gh25lsl9pc',
   youTubeId: 'mwdXBFtRl0o',
-  podbeanId: '',
+  podbeanId: 'e2iiw-6180fa',
   shortUrl: 'http://jsair.io/typed-fp',
   host: {
-    links: [
-    ],
-    tips: [
-    ],
     picks: [
+      `[React30](https://react30.com/) episode [006 - Origins of React](https://youtu.be/yiCnfJ1rflI) with [Jordan Walke](https://twitter.com/jordwalke)`,
+      `[Midwest JS](http://midwestjs.com/) next week - [tickets](https://ti.to/midwestjs/2016) are still available!`,
+      `My frontend masters workshops are next week! [Webpack Deep Dive](http://kcd.im/fem-webpack) and [Writing an Open Source JavaScript Library](http://kcd.im/fem-oss) - tickets are still available for in person and online.`,
+      `[Ragtime](https://en.wikipedia.org/wiki/Ragtime_(musical)) the musical`,
     ],
   },
   panelists: [
-    {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
+    {twitter: 'dan_abramov'},
     {
       twitter: 'drboolean',
-      links: [
-      ],
       tips: [
+        `Use an ide or vim setup`,
+        `Learn to reason about type signatures alone`,
+        `Constraints are great, but start simple and dial them up later`,
+        `Write simple code w/o abstractions until you feel the pain - then learn abstractions to solve that`,
       ],
       picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[Math primers](https://jeremykun.com/primers/)`,
+        `[What a bijection is not](https://github.com/twitter/bijection/issues/41)`,
+        `[Functional Geekery Susan Potter](https://www.functionalgeekery.com/episode-61-susan-potter)`,
       ],
     },
     {
       twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[Mies: Minimal ClojureScript template](https://github.com/swannodette/mies)`,
       ],
     },
   ],
