da6cf8fb2cf272d9d6b0697985bcbc890f227763
Kent C. Dodds <kent@doddsfamily.us>
2016-08-10 18:03:09 -0500

    Add links, tips, and picks to mentor show #117

--- episodes/2016-08-10/index.js
+++ episodes/2016-08-10/index.js
@@ -7,38 +7,55 @@ export default {
       links: [
       ],
       tips: [
+        `You will always know more than most people who started learning after you. So, don’t sell yourself short and give yourself room to be comfortable.`,
       ],
       picks: [
+        `[GEMconf](https://www.embermeetup.com/gemconf)`,
       ],
     },
     {
       name: 'Kim Crayton',
       twitter: 'KimCrayton1',
       links: [
+        `[JrDevMentoring.com](http://JrDevMentoring.com)`,
+        `[TechTalkForNonTechies.com](http://TechTalkForNonTechies.com)`,
+        `[CodeNewbie.org](http://www.codenewbie.org/podcast/impostor-syndrome) podcast on inposter syndrome`,
+        `[Stop Lying to Newbies](http://wp.me/p6PKEK-qc)`,
+        `[Getting the mentoring you need](http://vspa.berkeley.edu/sites/default/files/shared/doc/Workshop1_Public%20Slides.pdf)`,
       ],
       tips: [
+        `Mentoring Minutes`,
+        `Get comfortable with being uncomfortable`,
+        `Find opportunities to teach what you’ve learned`,
       ],
       picks: [
+        `[Harvard CS50](https://www.edx.org/course/introduction-computer-science-harvardx-cs50x)`,
+        `[CodeNewbie.org](http://codenewbie.org/)`,
+        `[Women Who Code](https://www.womenwhocode.com/)`,
+        `[Hood.ie](http://hood.ie/)`,
       ],
     },
     {
       name: 'Colt McAnlis',
       twitter: 'duhroach',
       links: [
+        `[On Getting the Mentoring You Need](https://amzn.com/1422196003)`,
       ],
       tips: [
+        `Learn Data Compression`,
+        `Take training on how to be a manager`,
       ],
       picks: [
+        `[Technical Management Program - UCLA Extension](http://tmp.uclaextension.edu/) `,
       ],
     },
     {
       name: 'Jed Watson',
       twitter: 'jedwatson',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `[Typography.js](https://kyleamathews.github.io/typography.js/)`,
+        `[KeystoneJS](http://keystonejs.com)`,
+        `[Node School](http://nodeschool.io/)`,
       ],
     },
   ],
@@ -47,88 +64,21 @@ export default {
   `,
   hangoutId: 'cld206bma155l4ku80dlm761k2c',
   youTubeId: 'lA8mo8oQ9J0',
-  podbeanId: '',
+  podbeanId: '2736i-61b46c',
   shortUrl: 'http://jsair.io/mentor',
   host: {
     links: [
+      `[Tweet inspiring this episode](https://twitter.com/zinyando/status/741533996895621120)`,
+      `[How I interview](https://github.com/kentcdodds/ama/issues/168)`,
     ],
     tips: [
+      `Spread things out, reduce commitments`,
+      `Make stuff public`,
     ],
     picks: [
+      `[react-workshop](https://github.com/mzabriskie/react-workshop) by [Matt Zabriskie](https://twitter.com/mzabriskie)`,
+      `[Open Source Workshop Slides](http://kcd.im/oss-workshop)`,
+      `[Webpack Workshop Slides](http://kcd.im/webpack-workshop)`,
     ],
   },
-  panelists: [
-    {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-  ],
 }
