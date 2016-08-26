af7270f0d8b2f6f57775b6e81be14881dceb6e31
Kent C. Dodds <kent@doddsfamily.us>
2016-08-25 22:02:25 -0600

    fix formatting of the react rally show

--- episodes/2016-08-25/index.js
+++ episodes/2016-08-25/index.js
@@ -8,11 +8,9 @@ export default {
   description: `
     We'll be live and on-site at React Rally. Join us as we chat with attendees and speakers about the awesome React conference in Utah.
     <br />
-- [Katherine Poulter](https://twitter.com/katpoulter)
-- [Sergio Cruz](https://twitter.com/hashtagserg)
-- [James Long](https://twitter.com/jlongster)
-- [Ryan Florence](https://twitter.com/ryanflorence)
-- [Jamison Dance](https://twitter.com/jergason)
+    [Katherine Poulter](https://twitter.com/katpoulter), [Sergio Cruz](https://twitter.com/hashtagserg),
+    [James Long](https://twitter.com/jlongster), [Ryan Florence](https://twitter.com/ryanflorence),
+    [Jamison Dance](https://twitter.com/jergason)
   `,
   youTubeId: 'XYBpRRkRvhM',
   podbeanId: 'rbgx5-621d84',
