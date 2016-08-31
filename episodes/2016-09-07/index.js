28a670d152392374d9dd610ac025a456629d098e
Kent C. Dodds <kent@doddsfamily.us>
2016-08-31 10:26:21 -0600

    add community show (#148)

---
+++ episodes/2016-09-07/index.js
@@ -0,0 +1,113 @@
+export default {
+  title: `Node.js and Community`,
+  guests: [
+    {
+      name: 'James M Snell',
+      twitter: 'jasnell',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      name: 'Myles Borins',
+      twitter: 'thealphanerd',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+  description: `
+    A huge part of open source is the community that is formed around it. This is one of the best parts of open source. It is also a challenge to manage, especially with big projects like Node.js. We'll be chatting with some community builders and code contributors to learn how Node.js fosters and grows its community.
+  `,
+  youTubeId: '2fMpu-yBxLM',
+  podbeanId: '',
+  shortUrl: 'http://jsair.io/community',
+  host: {
+    links: [
+    ],
+    tips: [
+    ],
+    picks: [
+    ],
+  },
+  panelists: [
+    {
+      twitter: 'dan_abramov',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'drboolean',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'getify',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'kwuchu',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'linclark',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'mzabriskie',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'pamasaur',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'tylermcginnis33',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+}
