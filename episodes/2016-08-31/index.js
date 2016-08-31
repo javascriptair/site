0075782c0a2e853bb175e9ab0cc8028bf559ed9b
Kent C. Dodds <kent@doddsfamily.us>
2016-08-31 13:39:51 -0600

    update typed show (#140)

--- episodes/2016-08-31/index.js
+++ episodes/2016-08-31/index.js
@@ -5,20 +5,23 @@ export default {
       name: 'Jeff Morrison',
       twitter: 'lbljeffmo',
       links: [
+        `A good introduction to variance: [Covariance and Contravariance](https://mikeash.com/pyblog/friday-qa-2015-11-20-covariance-and-contravariance.html) by [Mike Ash](https://mikeash.com/)`,
       ],
       tips: [
+        `Have a "just get it working" mentality. Then clean it up after`,
       ],
       picks: [
+        `[OCaml](https://ocaml.org/)`,
+        `[Reason](https://facebook.github.io/reason/)`,
+        `[Real World OCaml](https://realworldocaml.org/)`,
       ],
     },
     {
       name: 'Anders Hejlsberg',
       twitter: 'ahejlsberg',
-      links: [
-      ],
       tips: [
-      ],
-      picks: [
+        `Follow your passion. Go Play. Write some new code. That’s how you do your best work. Try to enjoy what you’re doing.`,
+        `There are lots of people coming to JS now. Lots of OOP programmings flowing into the ecosystem. JS can be a great FP language though. There’s a lot you can learn from studying FP in JS.`,
       ],
     },
   ],
@@ -26,87 +29,35 @@ export default {
     Adding typings to JavaScript is becoming more and more popular. We&#x27;re going to chat with the creators of TypeScript and Flow to discuss the why and how behind typed JavaScript.
   `,
   youTubeId: 'Qiqsg02nXFE',
-  podbeanId: '',
+  podbeanId: 'bz2fp-624493',
   shortUrl: 'http://jsair.io/typed',
   host: {
-    links: [
-    ],
     tips: [
+      `No matter how well you know something, keep your mind open to learn even more about it.`,
     ],
     picks: [
+      `[React Rally 2016 Notes Wiki](https://docs.google.com/document/d/14aUrWI0wcfmKi7Vjgek4z5gBjkPX9hvI2gSg8ZEjOMc/edit)`,
+      `[React events in depth](https://youtu.be/dRo_egw7tBc) live tomorrow!`,
+      `[husky](https://www.npmjs.com/package/husky) (used to use [ghooks](https://www.npmjs.com/package/ghooks))`,
     ],
   },
   panelists: [
     {
       twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `[Jest 15.0: New Defaults for Jest](http://facebook.github.io/jest/blog/2016/09/01/jest-15.html)`,
+        `[Grokking Algorithms](https://www.manning.com/books/grokking-algorithms)`,
       ],
     },
     {
       twitter: 'drboolean',
-      links: [
-      ],
       tips: [
+        `Functions should be input to output. Type systems help a lot more when you compose this way.`,
+        `Stay away from Object|Any :)`,
       ],
       picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[Jon Sterling - LambdaConf 2015 - Type Theory and its Meaning Explanations](https://www.youtube.com/watch?v=xMAqniX2Paw)`,
+        `[Chris Taylor - Algebra of Algebraic Data Types](http://chris-taylor.github.io/blog/2013/02/10/the-algebra-of-algebraic-data-types/)`,
       ],
     },
   ],
