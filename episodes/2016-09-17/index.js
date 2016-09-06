ee2b8d5ed9d1b695dc81c188d04f7c98d44d153f
Kent C. Dodds <kent@doddsfamily.us>
2016-09-06 14:04:10 -0600

    add strange loop show #46

---
+++ episodes/2016-09-17/index.js
@@ -0,0 +1,103 @@
+export default {
+  title: `On-site at The Strange Loop`,
+  guests: [
+    {
+      name: 'The Strange Loop',
+      twitter: 'strangeloop_stl',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+  description: `
+    We&#x27;re going to be chatting with the wonderful and intelligent people at The Strange Loop Conference!
+  `,
+  youTubeId: 'XPr9lT-UuTs',
+  podbeanId: '',
+  shortUrl: 'http://jsair.io/strange',
+  host: {
+    links: [
+    ],
+    tips: [
+    ],
+    picks: [
+    ],
+  },
+  panelists: [
+    {
+      twitter: 'dan_abramov',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'drboolean',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'getify',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'kwuchu',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'linclark',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'mzabriskie',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'pamasaur',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'tylermcginnis33',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+}
