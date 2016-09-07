80ea061c04b338dd80515e814818cf09e9f6469c
Kent C. Dodds <kent@doddsfamily.us>
2016-09-07 13:33:23 -0600

    add links, tips, and picks to community show (#148)

--- episodes/2016-09-07/index.js
+++ episodes/2016-09-07/index.js
@@ -4,41 +4,46 @@ export default {
     {
       name: 'James M Snell',
       twitter: 'jasnell',
-      links: [
-      ],
       tips: [
+        `Take Vacation! Put the Laptop Down! Go outside!`,
+        `Be Patient!`,
       ],
       picks: [
+        `We have Node.js developers in Fresno! [Fresnode.js Meetup](https://www.meetup.com/fresno-node-js), 2nd monday of every month. We’re looking for speakers!`,
       ],
     },
     {
       name: 'Gregor Martynus',
       twitter: 'gr2m',
-      links: [
-      ],
       tips: [
+        `Surround yourself with people that are good to you. Especially if you work remotely`,
+        `Invest into mentoring ([JavaScript Air Episode](https://jsair.io/mentor))`,
       ],
       picks: [
+        `New Podcast from [The Changelog](https://changelog.com/) by [Nadia Eghbal](https://twitter.com/nayafia) and [Mikeal Rodgers](https://twitter.com/mikeal): [Request for Commits](https://changelog.com/rfc/)`,
+        `[Offline Camp](http://offlinefirst.org/camp): Nov 4-7 in Santa Margarita, California`,
       ],
     },
     {
       name: 'Myles Borins',
       twitter: 'thealphanerd',
-      links: [
-      ],
       tips: [
+        `If you make a mistake genuinely apologize and move on. Stop engaging / don’t double down`,
+        `Show empathy for those you make stuff for, make stuff with, and for yourself`,
       ],
       picks: [
+        `[View Source Berlin](https://viewsourceconf.org/berlin-2016/)`,
+        `[Node Interactive Europe](http://events.linuxfoundation.org/events/node-interactive-europe)`,
       ],
     },
     {
       name: 'Tracy Hinds',
       twitter: 'hackygolucky',
-      links: [
-      ],
       tips: [
+        `When you feel something that you’re emotionally invested in, write a draft and take a second before you send it.`,
       ],
       picks: [
+        `[Operation Code](https://operationcode.org/)`,
       ],
     },
   ],
@@ -46,88 +51,19 @@ export default {
     A huge part of open source is the community that is formed around it. This is one of the best parts of open source. It is also a challenge to manage, especially with big projects like Node.js. We'll be chatting with some community builders and code contributors to learn how Node.js fosters and grows its community.
   `,
   youTubeId: '2fMpu-yBxLM',
-  podbeanId: '',
+  podbeanId: '2pwyh-627497',
   shortUrl: 'http://jsair.io/community',
   host: {
     links: [
+      `[Healthy Open Source](https://medium.com/the-javascript-collection/healthy-open-source-967fa8be7951) by [Mikeal Rogers](https://twitter.com/mikeal)`,
+      `[all-contributors specification](https://github.com/kentcdodds/all-contributors) (and the [CLI](https://github.com/jfmengels/all-contributors-cli)).`,
     ],
     tips: [
+      `If you feel like you know something really well, don’t be afraid to admit that you might be wrong or there may be something else better`,
     ],
     picks: [
+      `[Jest](http://facebook.github.io/jest/)`,
+      `[Free Jest lessons on Egghead.io](https://kcd.im/egghead-jest)`,
     ],
   },
-  panelists: [
-    {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-  ],
 }
