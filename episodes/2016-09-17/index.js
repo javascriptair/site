57e638f787d66fa6054c1f10fab3f64fbc583fa8
Kent C. Dodds <kent@doddsfamily.us>
2016-09-17 13:30:01 -0500

    add note about issues with audio. Closes #46

--- episodes/2016-09-17/index.js
+++ episodes/2016-09-17/index.js
@@ -13,10 +13,12 @@ export default {
     },
   ],
   description: `
-    We're going to be chatting with the wonderful and intelligent people at The Strange Loop Conference!
+    **NOTE**: We had a fantastic show this week at The Strange Loop! But Kent made a mistake by not setting my computer to use the microphone interface for the audio and so the sound is just using the MacBook's built-in microphone, which resulted in really terrible audio quality. Really sorry about this! On the audio feed, there is a rerun of one of the more popular shows that is relevant to the type of conversations you would hear at The Strange Loop.
+
+    **Original Description:** We're going to be chatting with the wonderful and intelligent people at The Strange Loop Conference!
   `,
   youTubeId: 'XPr9lT-UuTs',
-  podbeanId: '',
+  past: true,
   shortUrl: 'http://jsair.io/strange',
   host: {
     links: [
