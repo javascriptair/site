32f78e1d30e0f627ad7b02c0d9d55c39a6224e44
Kent C. Dodds <kent@doddsfamily.us>
2016-09-21 13:09:38 -0600

    add links, tips, picks, and publish cypress show #153

--- episodes/2016-09-21/index.js
+++ episodes/2016-09-21/index.js
@@ -4,21 +4,25 @@ export default {
     {
       name: 'Brian Mann',
       twitter: 'be_mann',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `Recent GitHub changes: [Projects Tab](https://help.github.com/articles/about-projects/) is awesome!`,
+        `[Electron](http://electron.atom.io/)`,
       ],
     },
     {
       name: 'Gleb Bahmutov',
       twitter: 'bahmutov',
       links: [
+        `[was-tested](https://github.com/bahmutov/was-tested)`,
+        `[cypress-image](https://hub.docker.com/r/bahmutov/cypress-image/)`,
+        `[multi-cypress](https://github.com/kensho/multi-cypress)`,
+        `[Zeit ▲ now](https://zeit.co/now/)`,
       ],
       tips: [
+        `Crash reporting (like TrackJs / Sentry / Raygun) are really useful during e2e testing!`,
       ],
       picks: [
+        `[Katacoda](https://www.katacoda.com) - online Docker and Kubernetes training in live containers.`,
       ],
     },
   ],
@@ -26,88 +30,15 @@ export default {
     Cypress.io is an automated testing tool that runs in the context of the browser and has some really solid features that make unit, integration, and even end to end testing much easier!
   `,
   youTubeId: 'JYPks6u2hGk',
-  podbeanId: '',
+  podbeanId: 'uwp6c-62de61',
   shortUrl: 'http://jsair.io/cypress',
   host: {
-    links: [
-    ],
     tips: [
+      `Don’t forget to set your audio input setting in Hangouts on Air… :(`,
     ],
     picks: [
+      `["Idealized Commit Logs: Code Simplification via Program Slicing"](https://www.youtube.com/watch?v=dSqLt8BgbRQ) by [Alan Shreve](https://twitter.com/inconshreveable)`,
+      `[split-guide](https://github.com/kentcdodds/split-guide) - A tool to help generate code for workshop repositories `,
     ],
   },
-  panelists: [
-    {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-  ],
 }
