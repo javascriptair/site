b75d7a6daeaca0db126634e5c9180c275af86162
Kent C. Dodds <kent@doddsfamily.us>
2016-09-29 09:16:47 -0600

    Add notes to web components show #150

--- episodes/2016-09-28/index.js
+++ episodes/2016-09-28/index.js
@@ -5,30 +5,41 @@ export default {
       name: 'Rob Dodson',
       twitter: 'rob_dodson',
       links: [
+        `Server Components (https://pimterry.github.io/server-components/)`,
+        `[Using React app techniques at the web component level with Redux, virtual-dom, and JSX ](https://component.kitchen/blog/posts/using-react-app-techniques-at-the-web-component-level-with-redux-virtual-dom-and-jsx)`,
+        `Dig up twitter thread. It’s a total centithread but here’s a decent jumping in point (https://twitter.com/dan_abramov/status/770645608415784960)`,
       ],
       tips: [
+        `The gas arrow in your rental car dashboard tells you which side the gas tank is on`,
       ],
       picks: [
+        `Slappy Cakes (http://www.slappycakes.com/)`,
+        `Elon Musk biography by Ashlee Vance (https://www.amazon.com/dp/B00KVI76ZS/ref=dp-kindle-redirect?_encoding=UTF8&btkr=1)`,
+        `The Corgi sticker from cotton bureau (https://cottonbureau.com/)`,
       ],
     },
     {
       name: 'Tushar Mathur',
       twitter: 'tusharmath',
       links: [
-      ],
-      tips: [
+        `[rwc](https://github.com/tusharmath/rwc)`,
+        `[Demythstifying Web Components](http://www.backalleycoder.com/2016/08/26/demythstifying-web-components/)`,
+        `[Funag](https://funag.github.io)`,
       ],
       picks: [
+        `[Google I/O 2012 - Breaking the JavaScript Speed Limit with V8](https://www.youtube.com/watch?v=UJPdhx5zTaw)`,
       ],
     },
     {
       name: 'Monica Dinculescu',
       twitter: 'notwaldorf',
-      links: [
-      ],
       tips: [
+        `[Emoji-cli](https://github.com/muan/emoji-cli) is an emoji-searcher-by-keyword as a cli`,
       ],
       picks: [
+        `Electron!`,
+        `GO VOTE`,
+        `[punchesbears](twitter.com/punchesbears) is an amazing twitter where a dude posts hilarious videos of various videogames he is making (dancing bears! Throw macbooks at hipsters! Drunk navigating of a 7-11 for a burrito!)`,
       ],
     },
   ],
@@ -36,87 +47,27 @@ export default {
     There's been quite a bit of talk about the Web Components spec lately. As with anything new, there's been a lot of fear, uncertainty, and doubt (FUD) about Web Components. We're going to chat about some of their use cases, strengths, and weaknesses to give us all a better idea of this tech.
   `,
   youTubeId: 'M_yEe9pVKgg',
-  podbeanId: '',
+  podbeanId: 'ikyrh-631dfc',
   shortUrl: 'http://jsair.io/web-components',
   host: {
-    links: [
-    ],
     tips: [
+      `Vote`,
+      `If you know [Elon Musk](https://twitter.com/elonmusk), I’d like an intro because [I want him on the show](https://twitter.com/kentcdodds/status/780968532976685056) I’m. Not. Joking.`,
     ],
     picks: [
+      `[slice-js so far...](https://gist.github.com/kentcdodds/e85254d4dbc1540df6b95eb9ad2098d9) (a gist giving a quick demo`,
+      `[React.js 50,000 stars](https://facebook.github.io/react/blog/2016/09/28/our-first-50000-stars.html) (and some awesome shirts for the whole family!)`,
     ],
   },
   panelists: [
     {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
       twitter: 'pamasaur',
-      links: [
-      ],
       tips: [
+        `Delete your Facebook (app)`,
       ],
       picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
+        `[NPR fact check](http://www.npr.org/2016/09/26/495115346/fact-check-first-presidential-debate)`,
+        `[Polymer Summit](https://www.polymer-project.org/summit/)`,
       ],
     },
   ],
---
+++ episodes/2016-10-05/index.js
@@ -0,0 +1,103 @@
+export default {
+  title: `TBA`,
+  guests: [
+    {
+      name: 'TBA',
+      twitter: '',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+  description: `
+    This show is yet to be announced, but you can bet it&#x27;ll be great!
+  `,
+  youTubeId: '5vqKnvutCug',
+  podbeanId: '',
+  shortUrl: 'http://jsair.io/2016-10-05',
+  host: {
+    links: [
+    ],
+    tips: [
+    ],
+    picks: [
+    ],
+  },
+  panelists: [
+    {
+      twitter: 'dan_abramov',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'drboolean',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'getify',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'kwuchu',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'linclark',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'mzabriskie',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'pamasaur',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+    {
+      twitter: 'tylermcginnis33',
+      links: [
+      ],
+      tips: [
+      ],
+      picks: [
+      ],
+    },
+  ],
+}
