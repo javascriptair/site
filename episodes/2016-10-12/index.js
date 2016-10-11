9cc16e2deb5ead92d0a66de7ecd009828451fea3
Kent C. Dodds <kent@doddsfamily.us>
2016-10-11 10:46:56 -0600

    add async patterns show #158

--- episodes/2016-10-12/index.js
+++ episodes/2016-10-12/index.js
@@ -1,9 +1,9 @@
 export default {
-  title: `TBA`,
+  title: `Async Patterns in JavaScript`,
   guests: [
     {
-      name: 'TBA',
-      twitter: '',
+      name: 'Peter Lyons',
+      twitter: 'focusaurus',
       links: [
       ],
       tips: [
@@ -13,11 +13,11 @@ export default {
     },
   ],
   description: `
-    This show is still being planned
+    One of the distinctive aspects of JavaScript is how it deals with asynchronous operations (like reading a file or making a request to a database or server). We're going to talk about the different patterns you can follow with async in JavaScript
   `,
   youTubeId: 'yBAC1k5nALU',
   podbeanId: '',
-  shortUrl: 'http://jsair.io/2016-10-12',
+  shortUrl: 'http://jsair.io/async',
   host: {
     links: [
     ],
