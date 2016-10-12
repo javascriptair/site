d026f037f47681df97e3e1a47dfa29081f4dd645
Kent C. Dodds <kent@doddsfamily.us>
2016-10-12 13:52:25 -0600

    add links, tips, picks, and podbeanID to async show #158

--- episodes/2016-10-12/index.js
+++ episodes/2016-10-12/index.js
@@ -5,20 +5,31 @@ export default {
       name: 'Valeri Karpov',
       twitter: 'code_barbarian',
       links: [
+        `The 80/20 Guide to ES2015 Generators, http://es2015generators.com/`,
+        `[Callback Hell is a Myth](http://thecodebarbarian.com/2015/03/20/callback-hell-is-a-myth)`,
       ],
       tips: [
+        `Think things through for yourself, don’t just take somebody’s word for it because they’re an "expert"`,
+        `Write about your learnings, whether on a blog or to your team, best way to learn is to teach someone else`,
       ],
       picks: [
+        `[PonyFoo ES6 in Depth](https://ponyfoo.com/articles/tagged/es6-in-depth)`,
       ],
     },
     {
       name: 'Peter Lyons',
       twitter: 'focusaurus',
       links: [
+        `[callbackhell.com](http://callbackhell.com/)`,
       ],
       tips: [
+        `Seek out mentors and peers`,
+        `Cultivate self-awareness for when your tools are not visual enough or not fast enough. Write custom tools/scripts early and often.`,
       ],
       picks: [
+        `[RegEx 101](https://regex101.com/)`,
+        `[Julia Evans' blog](http://jvns.ca) ([@b0rk on twitter](https://twitter.com/b0rk)`,
+        `Bouldering (rock climbing)`,
       ],
     },
   ],
@@ -26,88 +37,19 @@ export default {
     One of the distinctive aspects of JavaScript is how it deals with asynchronous operations (like reading a file or making a request to a database or server). We're going to talk about the different patterns you can follow with async in JavaScript
   `,
   youTubeId: 'yBAC1k5nALU',
-  podbeanId: '',
+  podbeanId: '6uak5-6384c1',
   shortUrl: 'http://jsair.io/async',
   host: {
     links: [
+      `[TC-39 finished proposals](https://github.com/tc39/proposals/blob/master/finished-proposals.md) (async/await is included in ES2017)`,
     ],
     tips: [
+      `Do what you think is right and what you want to do.`,
     ],
     picks: [
+      `[Yarn](https://yarnpkg.com/)`,
+      `[repl.it](http://repl.it/)`,
+      `[A Tale of Three Lists](https://github.com/getify/A-Tale-Of-Three-Lists)`,
     ],
   },
-  panelists: [
-    {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-  ],
 }
