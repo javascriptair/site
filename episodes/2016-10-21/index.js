fd158f41c9a5db8043e1780a2d241002f10c837b
Kent C. Dodds <kent@doddsfamily.us>
2016-10-22 08:33:36 -0600

    add guests and update connect.tech show

--- episodes/2016-10-21/index.js
+++ episodes/2016-10-21/index.js
@@ -3,104 +3,17 @@ export default {
   time: '12:30',
   timezone: 'ET',
   guests: [
-    {
-      name: 'Connect.tech',
-      twitter: 'connect_js',
-      squareImage: true,
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
+    {name: 'Stephanie Brubaker', twitter: 'sjbrubaker'},
+    {name: 'Tim Dorr', twitter: 'timdorr'},
+    {name: 'Ben Ilegbodu', twitter: 'benmvp'},
   ],
   description: `
-    We'll be live and on site at Connect.tech in Atlanta Georgia. See you there!
+    We'll be live and on site at [Connect.tech](https://twitter.com/connect_js) in Atlanta Georgia. See you there!
   `,
   youTubeId: 's9kNekW8B-8',
-  podbeanId: '',
+  podbeanId: 'ucpj3-63ccdd',
   shortUrl: 'http://jsair.io/connect',
-  host: {
-    links: [
-    ],
-    tips: [
-    ],
-    picks: [
-    ],
-  },
   panelists: [
-    {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
+    {twitter: 'drboolean'},
   ],
 }
