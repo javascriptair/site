bf660687a9c2b86ecee5fa5eaf8ed483df6bad2b
Kent C. Dodds <kent@doddsfamily.us>
2016-10-27 06:03:45 -0400

    update data for the React Native show (#154)

--- episodes/2016-10-26/index.js
+++ episodes/2016-10-26/index.js
@@ -4,31 +4,44 @@ export default {
     {
       name: 'Bonnie Eisenman',
       twitter: 'brindelle',
-      links: [
-      ],
       tips: [
+        `[Shubheksa's JavaScript Package Managers 101 post](https://medium.com/@shubheksha/javascript-package-managers-101-9afd926add0a#.lny9qf3cb)`,
+        `[Ariel Elkin's post, Why I'm not a React Native developer](https://arielelkin.github.io/articles/why-im-not-a-react-native-developer)`,
       ],
       picks: [
+        `[Botwiki](https://botwiki.org/), an open catalog / community "of friendly, useful, artistic online bots, and tools and tutorials that can help you make them."`,
       ],
     },
     {
       name: 'Tyler McGinnis',
       twitter: 'tylermcginnis33',
-      links: [
-      ],
-      tips: [
-      ],
       picks: [
+        `[React Training](http://reacttraining.com)`,
       ],
     },
     {
       name: 'Ken Wheeler',
       twitter: 'ken_wheeler',
       links: [
+        `[Exponent](https://getexponent.com/)`,
+        `[Victory](https://victory.formidable.com)`,
+        `[Victory UI Explorer](https://itunes.apple.com/us/app/victory-ui-explorer/id1163915348?mt=8)`,
       ],
       tips: [
+        '`./node_modules/react-native/packager/packager.sh start --resetCache`',
+        '`watchman watch-del-all`',
+        `[Try Objective C](http://tryobjectivec.codeschool.com/)`,
+        `[React Native Animation Book](http://browniefed.com/react-native-animation-book/)`,
+        `Upgrade often`,
       ],
       picks: [
+        `[React Native Responsive Styles](https://github.com/FormidableLabs/react-native-responsive-styles)`,
+        `[React Native Maps](https://github.com/airbnb/react-native-maps)`,
+        `[Mobx](https://github.com/mobxjs/mobx)`,
+        `[Victory](https://github.com/FormidableLabs/victory)`,
+        `[Flow](https://flowtype.org/)`,
+        `[Hot Tub Time Machine](http://www.imdb.com/title/tt1231587/)`,
+        `[McDonalds All Day Breakfast](http://www.usatoday.com/story/money/2016/07/06/mcdonalds-expands-all-day-breakfast-menu/86747130/)`,
       ],
     },
   ],
@@ -36,79 +49,7 @@ export default {
     JavaScript is taking the software world by storm, and we're going to talk about yet another enabling technology: [React Native](https://facebook.github.io/react-native/).
   `,
   youTubeId: '5vqKnvutCug',
-  podbeanId: '',
+  podbeanId: 'cy5ga-63fc80',
   shortUrl: 'http://jsair.io/react-native',
-  host: {
-    links: [
-    ],
-    tips: [
-    ],
-    picks: [
-    ],
-  },
-  panelists: [
-    {
-      twitter: 'dan_abramov',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'drboolean',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'getify',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'kwuchu',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'linclark',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'mzabriskie',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-    {
-      twitter: 'pamasaur',
-      links: [
-      ],
-      tips: [
-      ],
-      picks: [
-      ],
-    },
-  ],
+  host: {},
 }
